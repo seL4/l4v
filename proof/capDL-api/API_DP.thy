c060f715dbef1c4ede65abe4bf659c0d88427a9a
Andrew Boyton <andrew.boyton@nicta.com.au>
2014-07-24 19:56:24 +1000

    Add a top-level file for the capDL API proofs.

---
+++ proof/capDL-api/API_DP.thy
@@ -0,0 +1,15 @@
+(*
+ * Copyright 2014, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(NICTA_GPL)
+ *)
+
+theory API_DP
+imports Arch_DP TCB_DP CNode_DP IRQ_DP
+begin
+
+end
