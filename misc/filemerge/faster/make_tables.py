c58d1c615e2ca41b15c934552c3e1c1812015dff
Matthew Fernandez <matthew.fernandez@nicta.com.au>
2015-10-14 11:02:02 +1100

    misc/filemerge: Add an optimised version of this translator.
    Because sometimes light speed is too slow.

---
+++ misc/filemerge/faster/make_tables.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+#
+# Copyright 2015, NICTA
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(NICTA_BSD)
+#
+
+import argparse, os, sys
+
+MY_DIR = os.path.dirname(os.path.abspath(__file__))
+
+sys.path.append(os.path.join(MY_DIR, '../../../internal/misc/pysymbols'))
+import isasymbols
+
+def main(argv):
+    parser = argparse.ArgumentParser(
+        description='generate unicode-ascii translation tables')
+    parser.add_argument('--output', '-o', type=argparse.FileType('w'),
+        default=sys.stdout, help='output file')
+    options = parser.parse_args(argv[1:])
+
+    with open(os.path.join(MY_DIR, '../../../isabelle/etc/symbols')) as f:
+        t = isasymbols.Translator(f.read())
+
+    # Write an ASCII-to-unicode table.
+    options.output.write('static map<wstring, wchar_t> ascii_to_unicode = {\n')
+    for sym in t.symbols:
+        options.output.write('    { L"%s", L\'\\x%x\' },\n' %
+            (sym.ascii_text.replace('\\', '\\\\'), sym.code_point))
+    options.output.write('};\n\n')
+
+    # Write a unicode-to-ASCII table.
+    options.output.write('static map<wchar_t, wstring> unicode_to_ascii = {\n')
+    for s in t.symbols:
+        options.output.write('    { L\'\\x%x\', L"%s" },\n' %
+            (s.code_point, s.ascii_text.replace('\\', '\\\\')))
+    options.output.write('};\n\n')
+
+    # Work out the maximum length of an ASCII sequence.
+    ascii_seq_max = max(len(s.ascii_text) for s in t.symbols)
+    options.output.write('static const unsigned int ASCII_SEQ_MAX = %u;\n\n' %
+        ascii_seq_max)
+
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main(sys.argv))
---
+++ misc/filemerge/faster/translate.cpp
@@ -0,0 +1,187 @@
+/*
+ * Copyright 2015, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(NICTA_BSD)
+ *
+ */
+
+/* This program was written by a recovering C programmer. It likely has many
+ * things that will make C++ programmers cringe. FFTF.
+ */
+
+#include <cassert>
+#include <cwchar>
+#include <error.h>
+#include <errno.h>
+#include <fstream>
+#include <getopt.h>
+#include <iostream>
+#include <locale>
+#include <map>
+
+using namespace std;
+
+#define die(args...) error(-1, errno, args)
+
+#include "tables.hpp" /* generated */
+
+static int to_ascii(wifstream &in, wofstream &out) {
+    assert(in.is_open());
+    assert(out.is_open());
+
+    wchar_t c;
+    while (in.get(c)) {
+
+        const auto t = unicode_to_ascii.find(c);
+        if (t == unicode_to_ascii.end())
+            out << c;
+        else
+            out << t->second;
+
+    }
+
+    return 0;
+}
+
+static int to_unicode(wifstream &in, wofstream &out) {
+    assert(in.is_open());
+    assert(out.is_open());
+
+    wchar_t buffer[ASCII_SEQ_MAX + 1];
+    unsigned int index = 0;
+
+    wchar_t c;
+    while (in.get(c)) {
+
+        buffer[index++] = c;
+
+        if ((index == 1 && buffer[0] != '\\') ||
+            (index == 2 && buffer[1] != '<') ||
+            (index == 3 && buffer[2] == '^')) {
+            /* Dump the buffer. */
+            buffer[index] = '\0';
+            out << buffer;
+            index = 0;
+        } else if (buffer[index - 1] == '>') {
+            buffer[index] = '\0';
+            const auto t = ascii_to_unicode.find(buffer);
+            if (t == ascii_to_unicode.end())
+                die("unrecognised ASCII sequence \"%.*ls\"", (int)index,
+                    buffer);
+            else
+                out << t->second;
+            index = 0;
+        } else if (index == sizeof(buffer) - 1) {
+            die("too large ASCII sequence \"%.*ls...\" in source", (int)index,
+                buffer);
+        }
+    }
+
+    if (index > 0) {
+        /* There is some pending text in the buffer. */
+        buffer[index] = '\0';
+        out << buffer;
+    }
+
+    return 0;
+}
+
+class Options {
+    public:
+        wifstream input;
+        wofstream output;
+        enum {
+            TO_UNICODE = 0,
+            TO_ASCII,
+        } mode;
+
+        Options() : mode(TO_UNICODE) {}
+
+        ~Options() {
+            input.close();
+            output.close();
+        }
+};
+
+static int parse_args(int argc, char **argv, Options &options) {
+    while (true) {
+        static struct option opts[] = {
+            {"input", required_argument, 0, 'i'},
+            {"output", required_argument, 0, 'o'},
+            {"to-ascii", no_argument, 0, 'a'},
+            {"to-unicode", no_argument, 0, 'u'},
+            {0, 0, 0, 0},
+        };
+        int index = 0;
+
+        int c = getopt_long(argc, argv, "ai:o:u", opts, &index);
+
+        if (c == -1)
+            break;
+
+        switch (c) {
+            case 'a':
+                options.mode = Options::TO_ASCII;
+                break;
+
+            case 'i':
+                if (options.input.is_open())
+                    options.input.close();
+                options.input.open(optarg);
+                if (!options.input.is_open())
+                    die("failed to open %s", optarg);
+                break;
+
+            case 'o':
+                if (options.output.is_open())
+                    options.output.close();
+                options.output.open(optarg);
+                if (!options.output.is_open())
+                    die("failed to open %s", optarg);
+                break;
+
+            case 'u':
+                options.mode = Options::TO_UNICODE;
+                break;
+
+            default:
+                return -1;
+        }
+    }
+
+    if (!options.input.is_open())
+        options.input.open("/dev/stdin");
+    if (!options.output.is_open())
+        options.output.open("/dev/stdout");
+
+    return 0;
+}
+
+int main(int argc, char **argv) {
+
+    /* Switch to the user's native locale, which hopefully supports UTF-8. */
+    locale::global(locale(""));
+
+    Options options;
+
+    if (parse_args(argc, argv, options) != 0)
+        return -1;
+
+    switch (options.mode) {
+
+        case Options::TO_ASCII:
+            return to_ascii(options.input, options.output);
+
+        case Options::TO_UNICODE:
+            return to_unicode(options.input, options.output);
+
+        default:
+            assert(!"invalid mode?");
+    }
+
+    return 0;
+}
