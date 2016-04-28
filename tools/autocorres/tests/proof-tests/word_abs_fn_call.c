a6f8332d60323b64e65349417ac493f74ffbc27a
Japheth Lim <Japheth.Lim@nicta.com.au>
2016-04-28 17:01:13 +1000

    autocorres: add some missing WordAbstract rules.

--- tools/autocorres/tests/proof-tests/word_abs_fn_call.c
+++ tools/autocorres/tests/proof-tests/word_abs_fn_call.c
@@ -51,4 +51,44 @@ uint32_t bar4(int a, uint32_t b, uint8_t c)
             return foo4(a, b, foo2(1, 2, 3));
 }
 
+uint8_t foo5(int a, uint32_t b, uint8_t c)
+{
+            return a + b + c;
+}
+
+uint8_t bar5(int a, uint32_t b, uint8_t c)
+{
+            return foo(1, 2, foo(1, 2, 3));
+}
+
+uint32_t foo6(int a, uint32_t b, uint8_t c)
+{
+            return a + b + c;
+}
+
+uint32_t bar6(int a, uint32_t b, uint8_t c)
+{
+            return foo2(a, b, foo2(1, 2, 3));
+}
+
+uint8_t foo7(int a, uint32_t b, uint8_t c)
+{
+            return a + b + c;
+}
+
+uint8_t bar7(int a, uint32_t b, uint8_t c)
+{
+            return foo3(1, 2, foo3(1, 2, 3));
+}
+
+uint32_t foo8(int a, uint32_t b, uint8_t c)
+{
+            return a + b + c;
+}
+
+uint32_t bar8(int a, uint32_t b, uint8_t c)
+{
+            return foo4(a, b, foo2(1, 2, 3));
+}
+
 
