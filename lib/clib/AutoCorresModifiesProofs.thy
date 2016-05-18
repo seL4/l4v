2f0b484c7c34e7384a7ffdd8471ada30e6d99ef1
Japheth Lim <Japheth.Lim@nicta.com.au>
2016-05-18 15:10:04 +1000

    Add license tags for autocorres-crefine files

--- lib/clib/AutoCorresModifiesProofs.thy
+++ lib/clib/AutoCorresModifiesProofs.thy
@@ -1,3 +1,13 @@
+(*
+ * Copyright 2016, NICTA
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(NICTA_GPL)
+ *)
+
 theory AutoCorresModifiesProofs imports
   "../../lib/ml-helpers/TermPatternAntiquote"
   "../../tools/autocorres/AutoCorres"
