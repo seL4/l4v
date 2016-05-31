eed586f4d8dfe0cee04734f3f1b5517218c28f3e
Daniel Matichuk <daniel.matichuk@nicta.com.au>
2016-05-31 14:52:11 +1000

    autolevity: allow proof tracing level to be set with AUTOLEVITY environment variable
    AUTOLEVITY=1 -- trace lemma dependencies, proof start/end
    AUTOLEVITY=2 -- as above, also trace all dependent theorems per apply statement

--- lib/AutoLevity_Hooks.thy
+++ lib/AutoLevity_Hooks.thy
@@ -1,7 +1,12 @@
 theory AutoLevity_Hooks
 imports "AutoLevity_Base"
 begin
-              
-setup \<open>AutoLevity_Base.setup_command_hook {trace_apply = true}\<close>
+
+setup \<open>
+case getenv "AUTOLEVITY" of
+  "1" => AutoLevity_Base.setup_command_hook {trace_apply = false} 
+| "2" => AutoLevity_Base.setup_command_hook {trace_apply = true}
+| _ => I
+\<close>
 
 end
