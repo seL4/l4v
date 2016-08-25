80bc041b49a676b85431dfe792399843a2a16bdd
Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>
2016-08-25 10:29:45 +1000

    x64 invariants: TcbAcc_AI checking

--- proof/invariant-abstract/X64/ArchTcbAcc_AI.thy
+++ proof/invariant-abstract/X64/ArchTcbAcc_AI.thy
@@ -12,7 +12,7 @@ theory ArchTcbAcc_AI
 imports "../TcbAcc_AI"
 begin
 
-context Arch begin global_naming ARM
+context Arch begin global_naming X64
 
 named_theorems TcbAcc_AI_assms
 
@@ -20,17 +20,24 @@ lemmas cap_master_cap_simps =
   cap_master_cap_def[simplified cap_master_arch_cap_def, split_simps cap.split arch_cap.split]
 
 lemma cap_master_cap_arch_eqDs:
-  "cap_master_cap cap = cap.ArchObjectCap (arch_cap.PageCap ref rghts sz mapdata)
-     \<Longrightarrow> rghts = UNIV \<and> mapdata = None
-          \<and> (\<exists>rghts mapdata. cap = cap.ArchObjectCap (arch_cap.PageCap ref rghts sz mapdata))"
-  "cap_master_cap cap = cap.ArchObjectCap arch_cap.ASIDControlCap
-     \<Longrightarrow> cap = cap.ArchObjectCap arch_cap.ASIDControlCap"
-  "cap_master_cap cap = cap.ArchObjectCap (arch_cap.ASIDPoolCap pool asid)
-     \<Longrightarrow> asid = 0 \<and> (\<exists>asid. cap = cap.ArchObjectCap (arch_cap.ASIDPoolCap pool asid))"
-  "cap_master_cap cap = cap.ArchObjectCap (arch_cap.PageTableCap ptr data)
-     \<Longrightarrow> data = None \<and> (\<exists>data. cap = cap.ArchObjectCap (arch_cap.PageTableCap ptr data))"
-  "cap_master_cap cap = cap.ArchObjectCap (arch_cap.PageDirectoryCap ptr data2)
-     \<Longrightarrow> data2 = None \<and> (\<exists>data2. cap = cap.ArchObjectCap (arch_cap.PageDirectoryCap ptr data2))"
+  "cap_master_cap cap = ArchObjectCap (ASIDPoolCap pool asid)
+     \<Longrightarrow> asid = 0 \<and> (\<exists>asid. cap = ArchObjectCap (ASIDPoolCap pool asid))"
+  "cap_master_cap cap = ArchObjectCap ASIDControlCap
+     \<Longrightarrow> cap = ArchObjectCap ASIDControlCap"
+  "cap_master_cap cap = ArchObjectCap (IOPortCap first_port last_port)
+     \<Longrightarrow> first_port = 0 \<and> last_port = -1
+          \<and> (\<exists> first_port last_port. cap = ArchObjectCap (IOPortCap first_port last_port))"
+  "cap_master_cap cap = ArchObjectCap (PageCap ref rghts maptype sz mapdata)
+     \<Longrightarrow> maptype = VMNoMap \<and> rghts = UNIV \<and> mapdata = None
+          \<and> (\<exists>maptype rghts mapdata. cap = ArchObjectCap (PageCap ref rghts maptype sz mapdata))"
+  "cap_master_cap cap = ArchObjectCap (PageTableCap ptr data)
+     \<Longrightarrow> data = None \<and> (\<exists>data. cap = ArchObjectCap (PageTableCap ptr data))"
+  "cap_master_cap cap = ArchObjectCap (PageDirectoryCap ptr data)
+     \<Longrightarrow> data = None \<and> (\<exists>data. cap = ArchObjectCap (PageDirectoryCap ptr data))"
+  "cap_master_cap cap = ArchObjectCap (PDPointerTableCap ptr data)
+     \<Longrightarrow> data = None \<and> (\<exists>data. cap = ArchObjectCap (PDPointerTableCap ptr data))"
+  "cap_master_cap cap = ArchObjectCap (PML4Cap ptr data2)
+     \<Longrightarrow> data2 = None \<and> (\<exists>data2. cap = ArchObjectCap (PML4Cap ptr data2))"
   by (clarsimp simp: cap_master_cap_def
               split: cap.split_asm arch_cap.split_asm)+
 
@@ -52,28 +59,29 @@ lemma cap_master_cap_tcb_cap_valid_arch:
   by (simp add: cap_master_cap_def tcb_cap_valid_def tcb_cap_cases_def
                    valid_ipc_buffer_cap_def  is_cap_simps
             split: option.splits cap.splits arch_cap.splits
-                   Structures_A.thread_state.splits)
+                   thread_state.splits)
 
 lemma storeWord_invs[wp, TcbAcc_AI_assms]:
   "\<lbrace>in_user_frame p and invs\<rbrace> do_machine_op (storeWord p w) \<lbrace>\<lambda>rv. invs\<rbrace>"
 proof -
   have aligned_offset_ignore:
-    "\<And>l sz. l<4 \<Longrightarrow> p && mask 2 = 0 \<Longrightarrow>
+    "\<And>l sz. l<8 \<Longrightarrow> p && mask 3 = 0 \<Longrightarrow>
        p+l && ~~ mask (pageBitsForSize sz) = p && ~~ mask (pageBitsForSize sz)"
   proof -
     fix l sz
-    assume al: "p && mask 2 = 0"
-    assume "(l::word32) < 4" hence less: "l<2^2" by simp
-    have le: "2 \<le> pageBitsForSize sz" by (case_tac sz, simp_all)
+    assume al: "p && mask 3 = 0"
+    assume "(l::machine_word) < 8" hence less: "l<2^3" by simp
+    have le: "3 \<le> pageBitsForSize sz"
+      by (case_tac sz, simp_all add: pageBits_def ptTranslationBits_def)
     show "?thesis l sz"
       by (rule is_aligned_add_helper[simplified is_aligned_mask,
           THEN conjunct2, THEN mask_out_first_mask_some,
-          where n=2, OF al less le])
+          where n=3, OF al less le])
   qed
 
   show ?thesis
     apply (wp dmo_invs)
-    apply (clarsimp simp: storeWord_def invs_def valid_state_def)
+    apply (clarsimp simp: storeWord_def invs_def valid_state_def upto0_7_def)
     apply (fastforce simp: valid_machine_state_def in_user_frame_def
                assert_def simpler_modify_def fail_def bind_def return_def
                pageBits_def aligned_offset_ignore
