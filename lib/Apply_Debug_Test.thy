edcd32ba3ad0ab680a24f23e165d4f4f6f0d818c
Daniel Matichuk <daniel.matichuk@data61.csiro.au>
2017-02-13 13:34:45 +1100

    apply_debug: update test/tutorial

--- lib/Apply_Debug_Test.thy
+++ lib/Apply_Debug_Test.thy
@@ -10,7 +10,7 @@
  *)
 
 theory Apply_Debug_Test
-imports Apply_Debug
+imports Apply_Debug Apply_Trace_Cmd
 begin
 
 chapter \<open>Apply_Debug\<close>
@@ -28,14 +28,17 @@ text \<open>
  the proof state at the break point. This can allow for debugging methods without necessarily rewriting
  their implementation.
 
- Some interactive markup is given in jEdit, including showing in real-time the currently executing
- method. When printing a proof state (via apply_debug or continue) the associated breakpoint
- will be highlighted (or the final invoked method
- if the proof thread has finished). Note that this includes marking up an Eisbach definition.
+ Some interactive markup is given in jEdit: in real-time the currently executing
+ method has the "running" markup (similar to a forked proof), clicking on
+ an apply_debug or continue command will show the subsequent breakpoint that was hit,
+ clicking on a breakpoint will highlight all "continue" commands which stopped at that breakpoint.
+ Note that breakpoints inside of Eisbach definitions are highlighted as well.
 
  Finally the "finish" command will resume execution and ignore all further breakpoints.
 
- Outside of an apply_debug invocation the #break method is ignored.
+ Outside of an apply_debug invocation the #break method is ignored and any "continue" or "finish"
+ commands will throw a runtime error. The debug command that finally terminates the method
+ (either finish or continue) will exit the debugger and print "Final Result".
 \<close>
 
 method some_break = #break
@@ -46,8 +49,7 @@ lemma assumes B: B
   apply_debug (rule conjI, #break, some_break, #break, assumption?)+
   continue
   continue
-    apply (rule B)
-  continue
+    apply (rule B) (* this effect is saved *)
   finish
   apply assumption
   done
@@ -59,70 +61,33 @@ text \<open>
   invocation. By default apply_debug will only hit untagged breakpoints, if any tags are given
   then both untagged breakpoints and those matching the given tags will trigger a break.
   Each breakpoint can be given 0 or 1 tags.
-
-  For more fine-grained control, each "continue" may also be given a list of tags. This expands
-  the list of breakpoint tags that can be hit only for the duration of that particular continue.
 \<close>
 
 method my_conjI = (#break "conjI", rule conjI)
 method my_assumption = (#break "assumption", assumption)
 
-lemma "A \<Longrightarrow> A \<and> A \<and> A \<and> A"
+lemma "A \<Longrightarrow> A \<and> A \<and> A \<and> A \<and> A"
   apply_debug (my_conjI, my_assumption) (* no breakpoints hit *)
 
   apply_debug (tags "assumption")
-    (#break, my_conjI, my_assumption)
-    continue
-    continue
+    (#break, my_conjI, my_assumption) (* inline breakpoint *)
+    continue (* assumption breakpoint *)
+    finish (* goal finished *)
 
   apply_debug (tags "conjI")
-    (my_conjI, my_assumption)
-  continue (tags "assumption")
-  finish
+    (my_conjI, my_assumption) (* conjI is hit *)
+  continue (* assumption breakpoint is skipped *)
 
-
-  continue
-  finish
   apply_debug (tags "assumption", "conjI")
-    (my_conjI, my_assumption)
-  continue
-  finish
-  continue
+    (my_conjI, my_assumption) (* conjI is hit *)
+  continue (* assumption is hit *)
   finish
   by assumption
 
+section \<open>Arguments to Continue Command\<close>
 
-named_theorems rule3
-
-method bazz for C :: int uses rule1 rule2 declares rule3 =
-  (match premises in H[OF rule2]:"PROP G" for G \<Rightarrow> \<open>#break\<close>)
-
-
-
-schematic_goal Z:
-  assumes AC: "A \<Longrightarrow> C"
-  assumes BA: "B \<Longrightarrow> A"
-  assumes AB: "A \<Longrightarrow> B"
-  assumes A: "A"
-  assumes rule1: "ZZZ"
-  assumes A: "A"
-  assumes C
-  shows "(A \<Longrightarrow> ?C) \<Longrightarrow> D \<Longrightarrow> A \<and> B \<and> C"
-
-  apply (intro conjI)
-  prefer 2
-
-  apply_debug (match premises in H:"PROP C" for C \<Rightarrow>
-        \<open>#break, bazz 4 rule1: AB rule2: A\<close>)
-    term ?C (* C from match is bound *)
-    continue
-    term ?C (* C is now argument to bazz *)
-    term ?G (* G is bound from inner match from bazz *)
-    thm rule1 (* bound to AB from call site *)
-    thm rule2 (* bound to A from call site *)
-    thm H (* bound to match result from bazz (shadows outermost result) *)
-    finish
- oops
+text \<open>Continue can be given a single positive number that will skip over that many breakpoints.
+NB: continue 1 is the same as a bare continue.\<close>
 
 lemma
   assumes AB: "A \<Longrightarrow> B"
@@ -146,5 +111,116 @@ lemma
   continue 11
   continue
 done
+
+text \<open>
+  Continue may also be given a method, which is understood as a filter on each breakpoint.
+  If the method successfully applies, then the breakpoint is hit and the result of executing the
+  method is left as the proof state. If it fails at a given breakpoint, it is ignored and execution
+  continues, either until the next breakpoint or until the method terminates.
+  NB: "finish" is equivalent to "continue fail".
+
+  Note that a breakpoint alone cannot fail or affect backtracking directly.
+\<close>
+
+lemma
+  assumes B: B and C: C
+  shows "A \<or> D \<or> (B \<and> C)"
+  apply_debug ((rule disjI1 disjI2, #break)+, rule conjI, #break)
+  continue (rule B) (* skip until conjI reveals B *)
+   apply (rule C)
+   finish
+  done
+
+
+section \<open>Element binding\<close>
+
+text \<open>
+ Inside of a "continue" block we can refer to Eisbach variables that are in the
+ lexographical scope of the associated breakpoint.
+ This includes arguments passed to Eisbach methods and variables that are bound inside of a
+ "match" body.
+\<close>
+
+named_theorems rule3
+
+method bazz for C :: int uses rule1 rule2 declares rule3 =
+  (match premises in H[OF rule2]:"PROP G" for G \<Rightarrow> \<open>#break\<close>)
+
+schematic_goal Z:
+  assumes AC: "A \<Longrightarrow> C"
+  assumes BA: "B \<Longrightarrow> A"
+  assumes AB: "A \<Longrightarrow> B"
+  assumes A: "A"
+  assumes rule1: "ZZZ"
+  assumes A: "A"
+  assumes C
+  shows "(A \<Longrightarrow> ?C) \<Longrightarrow> D \<Longrightarrow> A \<and> B \<and> C"
+
+  apply (intro conjI)
+  prefer 2
+
+  apply_debug (match premises in H:"PROP C" for C \<Rightarrow>
+        \<open>#break, bazz 4 rule1: AB rule2: A\<close>)
+    term ?C (* C from match is bound *)
+    thm H (* H is bound as the (focused) fact proving C *)
+    continue (* inside bazz method now *)
+    term ?C (* C is now argument to bazz *)
+    term ?G (* G is bound from inner match from bazz *)
+    thm rule1 (* bound to AB from call site *)
+    thm rule2 (* bound to A from call site *)
+    thm H (* bound to match result from bazz (shadows original H) *)
+    finish
+ oops
+
+section \<open>Tracing used rules\<close>
+
+text \<open>
+ We can observe any used rules between breakpoints by passing apply_debug the "trace" flag. This
+ implicitly invokes apply_trace after each continue, showing the rules that have been used since
+ the last breakpoint. Note that this is subject to backtracking, so any rules that were used
+ as part of a backtracking branch that has since been discarded will not appear.
+\<close>
+
+lemma assumes A: A and B: B
+  shows "A \<and> B"
+  apply_debug (trace) ((rule conjI | rule A B), #break)+
+  continue
+  continue
+  finish
+  done
+
+text \<open>
+ The trace flag can be passed an argument string (in a cartouche) which accepts the same syntax
+ as find_theorems. This will filter the list of all used theorems shown.
+\<close>
+
+lemma assumes A: A and B: B
+  shows "A \<and> B"
+  apply_debug (trace \<open>"_ \<and> _"\<close>) ((rule conjI | rule A B), #break)+
+  continue
+  finish
+  done
+
+
+section \<open>Final Notes\<close>
+
+text \<open>
+ Breakpoints are subject to the real execution path of the method, which includes all backtracking
+ branches. This can result in potentially confusing traces when, for example, we see the same proof
+ state revisited multiple times.
+\<close>
+
+text \<open>
+ Only the "raw" proof state is lifted when continuing from a breakpoint: any additional context
+ information (e.g. from supply or other Isar commands) is discarded.
+\<close>
+
+text \<open>
+ The proof thread for the method is "stateful", shown by the "forked" markup in jEdit. Although
+ clicking on a continue will show you which breakpoint it hit, the actual state of the proof thread
+ could be further in the future. Any modifications to the proof state in the past
+ require this thread to be restarted in order to "rewind time" and account for the changes. This will
+ be reflected in the markup for the method.
+\<close>
 
 end
