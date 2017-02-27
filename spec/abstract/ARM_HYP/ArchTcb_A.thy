1f0988c725e35a3323c22767f92771b65686d354
Miki Tanaka <miki.tanaka@nicta.com.au>
2017-02-27 16:55:54 +1100

    arm-hyp abstract: ArchTcb_A

--- spec/abstract/ARM_HYP/ArchTcb_A.thy
+++ spec/abstract/ARM_HYP/ArchTcb_A.thy
@@ -1,11 +1,11 @@
 (*
- * Copyright 2014, General Dynamics C4 Systems
+ * Copyright 2017, Data61, CSIRO
  *
  * This software may be distributed and modified according to the terms of
  * the GNU General Public License version 2. Note that NO WARRANTY is provided.
  * See "LICENSE_GPLv2.txt" for details.
  *
- * @TAG(GD_GPL)
+ * @TAG(DATA61_GPL)
  *)
 
 (*
@@ -24,6 +24,13 @@ definition
   arch_tcb_set_ipc_buffer :: "machine_word \<Rightarrow> vspace_ref \<Rightarrow> (unit, 'a::state_ext) s_monad"
 where
   "arch_tcb_set_ipc_buffer target ptr \<equiv> as_user target $ set_register TPIDRURW ptr"
+
+definition
+  sanitise_register :: "tcb \<Rightarrow> register \<Rightarrow> machine_word \<Rightarrow> machine_word"
+where
+  "sanitise_register t r v \<equiv> case r of
+      CPSR \<Rightarrow> (v && 0xf8000000) || 0x150
+    | _    \<Rightarrow> v"
 
 end
 end
