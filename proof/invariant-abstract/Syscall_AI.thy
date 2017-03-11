db2d6077868130495b2263e3c6f12d16250a2aa7
Gerwin Klein <gerwin.klein@data61.csiro.au>
2017-03-11 15:08:46 +1100

    ainvs: fork slow proof

--- proof/invariant-abstract/Syscall_AI.thy
+++ proof/invariant-abstract/Syscall_AI.thy
@@ -530,11 +530,11 @@ lemma sts_mcpriority_tcb_at_ct[wp]:
 lemma sts_tcb_inv_wf [wp]:
   "\<lbrace>tcb_inv_wf i\<rbrace> set_thread_state t st \<lbrace>\<lambda>rv. tcb_inv_wf i\<rbrace>"
   apply (case_tac i)
-  apply (wp set_thread_state_valid_cap hoare_vcg_all_lift hoare_vcg_const_imp_lift
+  by (wp set_thread_state_valid_cap hoare_vcg_all_lift hoare_vcg_const_imp_lift
          | simp add: tcb_at_typ split: option.split
          | safe
          | wp sts_obj_at_impossible)+
-  done
+
 
 lemma sts_valid_inv[wp]:
   "\<lbrace>valid_invocation i\<rbrace> set_thread_state t st \<lbrace>\<lambda>rv. valid_invocation i\<rbrace>"
