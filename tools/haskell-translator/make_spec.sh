33a1fed708e546682a36e6b503801e82422313b9
Joel Beeren <joel.beeren@nicta.com.au>
2017-03-29 17:44:15 +1100

    x64: haskell-translator: make translator use whitespace removal tool

--- tools/haskell-translator/make_spec.sh
+++ tools/haskell-translator/make_spec.sh
@@ -130,3 +130,5 @@ for thy in $SPECNONARCH/${ARCHES[0]}/*.thy; do rsync -c "$thy" "$SPEC/"; done
 
 rm -r $SPECNONARCH
 rm -r $TMPFILE
+
+cd $TRANSLATOR/../.. && ./misc/whitespace/fix-commit-whitespace
