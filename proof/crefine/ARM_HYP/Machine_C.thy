d4f698f26076f9ca5eefa503f3f9a8cd3d25da92
Miki Tanaka <miki.tanaka@nicta.com.au>
2017-04-19 10:34:12 +1000

    arm-hyp crefine: fix return type in get_gic_vcpu_ctrl_hcr_ccorres

--- proof/crefine/ARM_HYP/Machine_C.thy
+++ proof/crefine/ARM_HYP/Machine_C.thy
@@ -307,7 +307,7 @@ assumes get_r12_fiq_ccorres:
 (* ARM Hypervisor Virtual Global Interrupt Controller (VGIC) *)
 
 assumes get_gic_vcpu_ctrl_hcr_ccorres:
-  "ccorres (op =) ret__unsigned_long_' \<top> UNIV []
+  "ccorres (op =) ret__unsigned_' \<top> UNIV []
            (doMachineOp get_gic_vcpu_ctrl_hcr) (Call get_gic_vcpu_ctrl_hcr_'proc)"
 assumes get_gic_vcpu_ctrl_vmcr_ccorres:
   "ccorres (op =) ret__unsigned_long_' \<top> UNIV []
