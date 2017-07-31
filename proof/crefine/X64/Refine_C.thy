387157583406d787415a038f50edc815ba21cece
Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>
2017-07-31 13:30:25 +1000

    x64: add crefine stubs to keep theory_imports happy

---
+++ proof/crefine/X64/Refine_C.thy
@@ -0,0 +1,19 @@
+(*
+ * Copyright 2017, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ *)
+
+chapter "Toplevel Refinement Statement"
+
+(* Stub to keep the theory_imports check happy
+   until we're ready to write some proofs here. *)
+
+theory Refine_C
+imports Main
+begin
+end
---
+++ proof/crefine/X64/SR_lemmas_C.thy
@@ -0,0 +1,17 @@
+(*
+ * Copyright 2017, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ *)
+
+(* Stub to keep the theory_imports check happy
+   until we're ready to write some proofs here. *)
+
+theory SR_lemmas_C
+imports Main
+begin
+end
