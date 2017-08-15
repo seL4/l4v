92f5d14c0b6747bcd44f282dcdedcc1f5d755071
Joel Beeren <joel.beeren@nicta.com.au>
2017-08-15 14:49:41 +1000

    x64: crefine: add Include_C

---
+++ proof/crefine/X64/Include_C.thy
@@ -0,0 +1,17 @@
+(*
+ * Copyright 2017, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ *)
+
+theory Include_C
+imports
+  "../../../spec/cspec/$L4V_ARCH/KernelInc_C"
+  "../../refine/$L4V_ARCH/Refine"
+begin
+
+end
