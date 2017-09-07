a680e93ffc839a21ea6c07356a2796b41d0538b5
Miki Tanaka <miki.tanaka@nicta.com.au>
2017-09-07 11:14:27 +1000

    rt invariants: fix add symrefs for RT

--- proof/invariant-abstract/InvariantsPre_AI.thy
+++ proof/invariant-abstract/InvariantsPre_AI.thy
@@ -198,6 +198,8 @@ datatype reftype
   = TCBBlockedSend | TCBBlockedRecv
      | TCBSignal | TCBBound | EPSend | EPRecv | NTFNSignal | NTFNBound
      | TCBHypRef | HypTCBRef
+     | TCBSchedContext | SCTcb | NTFNSchedContext | SCNtfn
+     | TCBReply | ReplyCaller | SCReply | ReplySchedContext
 
 primrec
  symreftype :: "reftype \<Rightarrow> reftype"
@@ -212,6 +214,14 @@ where
 | "symreftype NTFNBound       = TCBBound"
 | "symreftype TCBHypRef = HypTCBRef"
 | "symreftype HypTCBRef = TCBHypRef"
+| "symreftype TCBSchedContext = SCTcb"
+| "symreftype SCTcb       = TCBSchedContext"
+| "symreftype NTFNSchedContext = SCNtfn"
+| "symreftype SCNtfn       = NTFNSchedContext"
+| "symreftype TCBReply        = ReplyCaller"
+| "symreftype ReplyCaller     = TCBReply"
+| "symreftype SCReply         = ReplySchedContext"
+| "symreftype ReplySchedContext = SCReply"
 
 
 definition
