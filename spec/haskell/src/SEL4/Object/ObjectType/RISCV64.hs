82436e19af13813237d2642677eebbb69007bd3b
Rafal Kolanski <rafal.kolanski@nicta.com.au>
2018-06-15 18:52:24 +1000

    riscv haskell: deriveCap and similar

--- spec/haskell/src/SEL4/Object/ObjectType/RISCV64.hs
+++ spec/haskell/src/SEL4/Object/ObjectType/RISCV64.hs
@@ -33,34 +33,42 @@ import qualified SEL4.API.Types.RISCV64 as Arch.Types
 {- Copying and Mutating Capabilities -}
 
 deriveCap :: PPtr CTE -> ArchCapability -> KernelF SyscallError Capability
-
 -- It is not possible to copy a page table or page directory capability unless
 -- it has been mapped.
-
-deriveCap = error "FIXME RISCV TODO"
+deriveCap _ (c@PageTableCap { capPTMappedAddress = Just _ }) = return $ ArchObjectCap c
+deriveCap _ (PageTableCap { capPTMappedAddress = Nothing })
+    = throw IllegalOperation
+-- Page capabilities are copied without their mapping information, to allow
+-- them to be mapped in multiple locations.
+deriveCap _ (c@FrameCap {})
+    = return $ ArchObjectCap $ c { capFMappedAddress = Nothing }
+-- ASID capabilities can be copied without modification
+deriveCap _ c@ASIDControlCap = return $ ArchObjectCap c
+deriveCap _ (c@ASIDPoolCap {}) = return $ ArchObjectCap c
 
 isCapRevocable :: Capability -> Capability -> Bool
-isCapRevocable newCap srcCap = error "FIXME RISCV TODO"
+isCapRevocable newCap srcCap = False
 
 updateCapData :: Bool -> Word -> ArchCapability -> Capability
-updateCapData = error "FIXME RISCV TODO"
+updateCapData _ _ c = ArchObjectCap c
+
+-- these seem to refer to extraction of fields from seL4_CNode_CapData
 
 cteRightsBits :: Int
-cteRightsBits = error "FIXME RISCV TODO"
+cteRightsBits = 0
 
 cteGuardBits :: Int
-cteGuardBits = error "FIXME RISCV TODO"
+cteGuardBits = 58
 
 -- Page capabilities have read and write permission bits, which are used to
 -- restrict virtual memory accesses to their contents. Note that the ability to
 -- map objects into a page table or page directory is granted by possession of
 -- a capability to it; there is no specific permission bit restricting this
 -- ability.
+-- FIXME RISCV does not mask any rights unlike other platforms, investigate whether that is intentional
 
 maskCapRights :: CapRights -> ArchCapability -> Capability
-maskCapRights = error "FIXME RISCV TODO"
-
--- FIXME RISCV TODO
+maskCapRights _ c = ArchObjectCap c
 
 {- Deleting Capabilities -}
 
@@ -110,15 +118,21 @@ performInvocation = error "FIXME RISCV TODO"
 {- Helper Functions -}
 
 capUntypedPtr :: ArchCapability -> PPtr ()
-capUntypedPtr = error "FIXME RISCV TODO"
+capUntypedPtr (FrameCap { capFBasePtr = PPtr p }) = PPtr p
+capUntypedPtr (PageTableCap { capPTBasePtr = PPtr p }) = PPtr p
+capUntypedPtr ASIDControlCap = error "ASID control has no pointer"
+capUntypedPtr (ASIDPoolCap { capASIDPool = PPtr p }) = PPtr p
+
+asidPoolBits :: Int
+asidPoolBits = 12
 
 capUntypedSize :: ArchCapability -> Word
-capUntypedSize = error "FIXME RISCV TODO"
-
--- FIXME RISCV TODO
+capUntypedSize (FrameCap {capFSize = sz}) = 1 `shiftL` pageBitsForSize sz
+capUntypedSize (PageTableCap {}) = 1 `shiftL` ptBits
+capUntypedSize (ASIDControlCap {}) = 0
+capUntypedSize (ASIDPoolCap {}) = 1 `shiftL` asidPoolBits
 
 -- No arch-specific thread deletion operations needed on RISC-V platform.
 
--- FIXME RISCV unchecked copypasta
 prepareThreadDelete :: PPtr TCB -> Kernel ()
 prepareThreadDelete _ = return ()
