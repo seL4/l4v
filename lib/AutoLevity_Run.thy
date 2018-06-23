4606b9ed6d9b1ab9ae99117f403f6249db314ed0
Japheth Lim <Japheth.Lim@data61.csiro.au>
2018-06-24 03:13:55 +1000

    lib: write one combined output file from AutoLevity_Run
    The output file is still placed in Lib for now. Needs further adjustment.

--- lib/AutoLevity_Run.thy
+++ lib/AutoLevity_Run.thy
@@ -12,30 +12,69 @@ theory AutoLevity_Run
 imports AutoLevity_Theory_Report
 begin
 
-
 ML \<open>
-fun get_report thy =
+fun levity_dump_for thy_name: string list option =
 let
-
-  val thy_nm = Context.theory_name thy;
-  val _ = @{print} ("Reporting on " ^ thy_nm ^ "...")
+  val thy = Thy_Info.get_theory thy_name;
+  val thy_short_name = Context.theory_name thy;
 
   val trans = AutoLevity_Base.get_transactions ();
+in if Symtab.defined trans thy_short_name then
+     AutoLevity_Theory_Report.get_reports_for_thy thy
+     |> AutoLevity_Theory_Report.string_reports_of
+     |> SOME
+   else
+     NONE
+end;
 
-  val _ = if Symtab.defined trans (Context.theory_name thy) then () else raise Option;
+fun levity_dump_all output_path: unit =
+let
+  val this_session = Session.get_name ();
+  val temp_path = File.tmp_path output_path;
 
-  val file_path = Path.append (Resources.master_directory thy) (Path.basic (thy_nm ^ ".lev"));
+  (* Wrap individual theory reports in JSON dictionaries
+     indexed by session name and theory name.
+     Note that this closes and re-opens the output file
+     over and over. But it's better than building the entire
+     combined output in memory. *)
+  fun dump_all _ [] = ()
+    | dump_all (first: bool) (thy_name::remaining) = let
+        val _ = @{print} ("Reporting on " ^ thy_name ^ "...")
+        val separator = if first then "" else ", "; (* from previous item *)
+        val json_key = [separator ^ quote thy_name ^ ":"];
+        in case levity_dump_for thy_name of
+              NONE => (
+                @{print} ("No transaction record for " ^ thy_name);
+                dump_all first remaining
+                )
+            | SOME thy_report => (
+                File.append_list temp_path (json_key @ thy_report);
+                dump_all false remaining
+                )
+        end;
 
-  val reports = AutoLevity_Theory_Report.get_reports_for_thy thy;
-  val lines = AutoLevity_Theory_Report.string_reports_of reports;
-
-  val _ = File.write_list file_path lines;
-
-in () end handle Option =>
-  ((@{print} ("No transaction record for " ^ (Context.theory_name thy)); ()))
+  val thy_names = Thy_Info.get_names ();
+  val _ = File.write_list temp_path ["{\n", quote this_session ^ ":\n", "{\n"];
+  val _ = dump_all true thy_names;
+  val _ = File.append_list temp_path ["}\n", "}"];
+  val _ = OS.FileSys.rename {
+            old = Path.implode (Path.expand temp_path),
+            new = Path.implode (Path.expand output_path)
+          };
+in
+    @{print} ("Report written to: " ^ Path.implode output_path);
+    ()
+end;
 \<close>
-ML \<open>val theories = Thy_Info.get_names () |> map Thy_Info.get_theory\<close>
 
-ML \<open>map get_report theories\<close>
+ML \<open>
+let
+  val this_session = Session.get_name ();
+  val output_path = Path.basic (this_session ^ ".lev")
+                    |> Path.append (Resources.master_directory @{theory})
+in
+  levity_dump_all output_path
 end
+\<close>
 
+end
