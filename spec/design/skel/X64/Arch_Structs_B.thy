e82c7a5c50fe53268c6968e092724fa2c03bbc1e
Gerwin Klein <gerwin.klein@data61.csiro.au>
2018-07-15 16:29:32 +0100

    x64 design: clean out old FIXMEs

--- spec/design/skel/X64/Arch_Structs_B.thy
+++ spec/design/skel/X64/Arch_Structs_B.thy
@@ -15,11 +15,9 @@ chapter "Common, Architecture-Specific Data Types"
 theory Arch_Structs_B
 imports "~~/src/HOL/Main" "../../../spec/machine/Setup_Locale"
 begin
-(* FIXME: Clagged from X64 version *)
 
 context Arch begin global_naming X64_H
 
-(* FIXME: add an underscore after translation of X64 prefix? *)
 #INCLUDE_HASKELL SEL4/Model/StateData/X64.lhs CONTEXT X64_H ONLY X64VSpaceRegionUse
 
 end (* context X64 *)
