53fde5e5ff134484aa9f4454f0c5a1183d4362dd
Gerwin Klein <gerwin.klein@data61.csiro.au>
2018-07-29 09:58:49 +0200

    lib/design: enable more Haskell-like list comprehension syntax
    Accept "[f x | x \leftarrow t]" in addition to "[f x . x \leftarrow t]",
    because the former is what naturally comes out of the Haskell translator, and
    the regexps that would be necessary in the Haskell translator for this are
    distasteful.
    
    JIRA-VER 927

--- spec/design/skel/X64/ArchRetype_H.thy
+++ spec/design/skel/X64/ArchRetype_H.thy
@@ -20,16 +20,7 @@ begin
 
 context Arch begin global_naming X64_H
 
-(* FIXME haskell-translator: VER-927 *)
-defs setIOPortMask_def:
-"setIOPortMask f l val \<equiv> (do
-    ports \<leftarrow> gets (x64KSAllocatedIOPorts \<circ> ksArchState);
-    ports' \<leftarrow> return $ ports aLU [(i,val). i \<leftarrow> [f .e. l]];
-    modify (\<lambda> s. s \<lparr>
-        ksArchState := (ksArchState s) \<lparr> x64KSAllocatedIOPorts := ports' \<rparr>\<rparr>)
-od)"
-
-#INCLUDE_HASKELL SEL4/Object/ObjectType/X64.lhs CONTEXT X64_H Arch.Types=ArchTypes_H ArchInv=ArchRetypeDecls_H NOT setIOPortMask bodies_only
+#INCLUDE_HASKELL SEL4/Object/ObjectType/X64.lhs CONTEXT X64_H Arch.Types=ArchTypes_H ArchInv=ArchRetypeDecls_H NOT bodies_only
 #INCLUDE_HASKELL SEL4/API/Invocation/X64.lhs CONTEXT X64_H bodies_only
 
 end (* context X64 *)
