423606ae60b80d8c854724eac51fcdbdf5293cc1
Michael Norrish <Michael.Norrish@data61.csiro.au>
2018-08-22 16:33:52 +1000

    c-parser: always attach GCC attributes to vars.
    Some attributes attached to global variables weren't kept in
    the AST if they appeared at the front of the declaration rather
    than the back.
    
    For instance, the aligned attribute was lost in this declaration:
       int __attribute__((aligned(16))) x;
    but kept if it appeared last:
       int y __attribute__((aligned(16)));
    
    Now fixed.

---
+++ tools/c-parser/testfiles/attributes.thy
@@ -0,0 +1,39 @@
+(*
+ * Copyright 2017, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ *)
+
+theory attributes
+  imports "../CTranslation"
+begin
+
+install_C_file "attributes.c"
+
+ML {*
+local
+open ProgramAnalysis
+in
+fun global_details vi = (srcname vi, length (get_vi_attrs vi))
+
+val all_global_details = get_globals #> map global_details
+end
+*}
+
+ML {*
+val results = CalculateState.get_csenv @{theory} "attributes.c"
+  |> the
+  |> all_global_details
+  |> sort (prod_ord string_ord int_ord)
+*}
+
+ML {*
+val _ = if results = [("u",1), ("v", 1)] then ()
+        else error "Test case failure"
+*}
+
+end
