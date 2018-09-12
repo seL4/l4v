e82cdd149c953c552be702d82544e88bc7a0c8aa
Edward Pierzchalski <ed.pierzchalski@data61.csiro.au>
2018-09-12 15:15:23 +1000

    lib: add method to shorthand larger methods
    Adds the `supply_local_method` command and `local_method` methods,
    which store and apply methods as a way to shorten repeated
    references to large or complicated methods.

---
+++ lib/Local_Method.thy
@@ -0,0 +1,59 @@
+(*
+ * Copyright 2018, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ *)
+
+theory Local_Method
+imports Main
+keywords "supply_local_method" :: prf_script % "proof"
+begin
+
+text \<open>See documentation in @{file Local_Method_Tests.thy}.\<close>
+
+ML {*
+  structure MethodData = Proof_Data(
+    type T = Method.method Symtab.table
+    val init = K Symtab.empty);
+*}
+
+method_setup local_method = \<open>
+  Scan.lift Parse.liberal_name >>
+  (fn name => fn _ => fn facts => fn (ctxt, st) =>
+    case (ctxt |> MethodData.get |> Symtab.lookup) name of
+        SOME method => method facts (ctxt, st)
+      | NONE => Seq.succeed (Seq.Error (K ("Couldn't find method text named " ^ quote name))))
+\<close>
+
+ML \<open>
+local
+
+val parse_name_text_ranges =
+  Scan.repeat1 (Parse.liberal_name --| Parse.!!! @{keyword "="} -- Method.parse)
+
+fun supply_method_cmd name_text_ranges ctxt =
+  let
+    fun add_method ((name, (text, range)), ctxt) =
+      let
+        val _ = Method.report (text, range)
+        val method = Method.evaluate text ctxt
+      in
+        MethodData.map (Symtab.update (name, method)) ctxt
+      end
+  in
+    List.foldr add_method ctxt name_text_ranges
+  end
+
+val _ =
+  Outer_Syntax.command @{command_keyword\<open>supply_local_method\<close>}
+    "Add a local method alias to the current proof context"
+    (parse_name_text_ranges >> (Toplevel.proof o Proof.map_context o supply_method_cmd))
+
+in end
+\<close>
+
+end
---
+++ lib/Local_Method_Tests.thy
@@ -0,0 +1,89 @@
+(*
+ * Copyright 2018, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the BSD 2-Clause license. Note that NO WARRANTY is provided.
+ * See "LICENSE_BSD2.txt" for details.
+ *
+ * @TAG(DATA61_BSD)
+ *)
+
+theory Local_Method_Tests
+imports
+  Eisbach_Methods
+begin
+
+text \<open>
+  @{command supply_local_method} allows adding named aliases for methods to the local
+  proof context, for when duplicating methods would be too difficult to maintain or debug
+  but refactoring the proof would take too long.
+
+  Usage:
+    supply_local_method my_name = <method text>
+
+    supply_local_method my_name_1 = <method text 1>
+                        my_name_2 = <method text 2>
+                        ...
+
+  Where <method text> uses the same syntax as the input to @{command apply}.
+\<close>
+
+text \<open>
+  @{method local_method} allows using a previously supplied local method to the current proof
+  state.
+
+  Usage:
+    apply (local_method name)
+\<close>
+
+experiment
+begin
+  section \<open>Basic Examples\<close>
+
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<or> B"
+    supply_local_method my_simp = simp
+    apply (local_method my_simp)
+    done
+
+  text \<open>Uses standard @{command apply} parser, allowing all the normal combinators\<close>
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<and> B"
+    supply_local_method my_slightly_complicated_method = (rule conjI, assumption+)
+    apply (local_method my_slightly_complicated_method)
+    done
+
+  text \<open>Can supply multiple methods\<close>
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<and> B"
+    supply_local_method my_rule = (rule conjI)
+                        my_asm = assumption+
+    apply (local_method my_rule)
+    apply (local_method my_asm)
+    done
+
+  section \<open>Failure Modes\<close>
+
+  text \<open>Fails on non-existent arguments\<close>
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<and> B"
+    supply_local_method foo = simp
+    apply (fails \<open>local_method bar\<close>)
+    sorry
+
+  text \<open>Doesn't persist between subgoals\<close>
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<and> B"
+    apply (rule conjI)
+     subgoal
+     supply_local_method my_simp = simp
+     apply (local_method my_simp)
+     done
+    apply (fails \<open>local_method my_simp\<close>)
+    sorry
+
+  text \<open>Does see top-level method definitions\<close>
+  method foo = simp
+  lemma "A \<Longrightarrow> B \<Longrightarrow> A \<and> B"
+    supply_local_method magic = foo
+    apply (local_method magic)
+    done
+
+end
+
+end
