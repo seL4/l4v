252991e9a73a881868d8aac4b9a6b246af395aa6
Japheth Lim <Japheth.Lim@data61.csiro.au>
2018-12-13 18:29:15 +1100

    CamkesCdlRefine: update policy_of to work with GrantReply

--- camkes/cdl-refine/Policy_CAMKES_CDL.thy
+++ camkes/cdl-refine/Policy_CAMKES_CDL.thy
@@ -18,6 +18,7 @@ begin
 
 text \<open>Defining suitable access policies from CAmkES compositions.\<close>
 
+(* TODO: remove these abbrevs *)
 abbreviation "edge_subject \<equiv> fst"
 abbreviation "edge_auth \<equiv> fst o snd"
 abbreviation "edge_object \<equiv> snd o snd"
@@ -35,29 +36,55 @@ definition
   policy_of :: "assembly \<Rightarrow> label auth_graph"
 where
   "policy_of spec \<equiv>
-     (* Every label has every authority over itself. *)
+     \<comment> \<open>Every label has every authority over itself.\<close>
      {edge. edge_subject edge \<in> fst ` set (components (composition spec)) \<and>
             edge_subject edge = edge_object edge} \<union>
 
-     (* Senders on seL4RPC connections. *)
+     \<comment> \<open>FIXME: policy_wellformed makes DeleteDerived too transitive.
+         For now, we relax it even further and say that reply caps from any component
+         can end up in any other component. Need to look into this at some point.\<close>
+     {edge. \<exists>c1 \<in> fst ` set (components (composition spec)).
+            \<exists>c2 \<in> fst ` set (components (composition spec)).
+            edge_subject edge = c1 \<and> edge_auth edge = DeleteDerived \<and>
+            edge_object edge = c2} \<union>
+
+     \<comment> \<open>Senders on seL4RPC connections.\<close>
      {edge. \<exists>from. from \<in> fst ` set (components (composition spec)) \<and>
                    (\<exists>conn \<in> set (connections (composition spec)).
                       from \<in> set (from_components (snd conn)) \<and>
                       conn_type (snd conn) = seL4RPC \<and>
                       edge_object edge = fst conn) \<and>
                     edge_subject edge = from \<and>
-                    edge_auth edge \<in> {Receive, Reset, SyncSend}} \<union>
+                    edge_auth edge \<in> {Reset, SyncSend, Call}} \<union>
 
-     (* Receivers on seL4RPC connections. *)
+     \<comment> \<open>Receivers on seL4RPC connections.\<close>
      {edge. \<exists>to. to \<in> fst ` set (components (composition spec)) \<and>
                  (\<exists>conn \<in> set (connections (composition spec)).
                     to \<in> set (to_components (snd conn)) \<and>
                     conn_type (snd conn) = seL4RPC \<and>
                     edge_object edge = fst conn) \<and>
                   edge_subject edge = to \<and>
-                  edge_auth edge \<in> {Receive, Reset, SyncSend}} \<union>
+                  edge_auth edge \<in> {Receive, Reset}} \<union>
 
-     (* Senders on seL4Asynch connections. *)
+     \<comment> \<open>Reply rights for seL4RPC connections.
+         Note that these rights are between pairs of components,
+         not between components and connections.\<close>
+     {edge. \<exists>from \<in> fst ` set (components (composition spec)).
+              \<exists>conn \<in> set (connections (composition spec)).
+                  conn_type (snd conn) = seL4RPC \<and>
+                  from \<in> set (from_components (snd conn)) \<and>
+                  (\<exists>to \<in> set (to_components (snd conn)).
+                      edge_subject edge = from \<and> edge_auth edge \<in> {DeleteDerived} \<and>
+                      edge_object edge = to)} \<union>
+     {edge. \<exists>from \<in> fst ` set (components (composition spec)).
+              \<exists>conn \<in> set (connections (composition spec)).
+                  conn_type (snd conn) = seL4RPC \<and>
+                  from \<in> set (from_components (snd conn)) \<and>
+                  (\<exists>to \<in> set (to_components (snd conn)).
+                      edge_subject edge = to \<and> edge_auth edge \<in> {Reply} \<and>
+                      edge_object edge = from)} \<union>
+
+     \<comment> \<open>Senders on seL4Asynch connections.\<close>
      {edge. \<exists>from. from \<in> fst ` set (components (composition spec)) \<and>
                    (\<exists>conn \<in> set (connections (composition spec)).
                       from \<in> set (from_components (snd conn)) \<and>
@@ -66,7 +93,7 @@ where
                     edge_subject edge = from \<and>
                     edge_auth edge \<in> {Notify, Reset}} \<union>
 
-     (* Receivers on seL4Asynch connections. *)
+     \<comment> \<open>Receivers on seL4Asynch connections.\<close>
      {edge. \<exists>to. to \<in> fst ` set (components (composition spec)) \<and>
                  (\<exists>conn \<in> set (connections (composition spec)).
                     to \<in> set (to_components (snd conn)) \<and>
@@ -75,9 +102,9 @@ where
                   edge_subject edge = to \<and>
                   edge_auth edge \<in> {Receive, Reset}} \<union>
 
-     (* Senders and recievers on dataports.
-        Here, we always assume both sides have Read and Write because
-        most of the dataport implementations use in-line signalling. *)
+     \<comment> \<open>Senders and recievers on dataports.
+         Here, we always assume both sides have Read and Write because
+         most of the dataport implementations use in-line signalling.\<close>
      {edge. \<exists>from. from \<in> fst ` set (components (composition spec)) \<and>
                    (\<exists>conn \<in> set (connections (composition spec)).
                       from \<in> set (from_components (snd conn)) \<and>
