22f2a1c08c3630e91a5baba314f21bd1c91208a9
Japheth Lim <Japheth.Lim@data61.csiro.au>
2019-03-21 19:54:16 +1100

    CSpec: update KernelInc_C imports to match seL4 build system changes
    This fixes import breakage due to seL4 commit f85774e36f.

--- spec/cspec/KernelInc_C.thy
+++ spec/cspec/KernelInc_C.thy
@@ -13,8 +13,8 @@ imports
   "Substitute"
   "c/build/$L4V_ARCH/generated/arch/object/structures_defs"
   "c/build/$L4V_ARCH/generated/arch/object/structures_proofs"
-  "c/build/$L4V_ARCH/generated/mode/api/shared_types_defs"
-  "c/build/$L4V_ARCH/generated/mode/api/shared_types_proofs"
+  "c/build/$L4V_ARCH/generated/sel4/shared_types_defs"
+  "c/build/$L4V_ARCH/generated/sel4/shared_types_proofs"
 begin
 
 end
