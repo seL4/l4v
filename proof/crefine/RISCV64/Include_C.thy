d0dade06f41c311f29af527cb44d549217e3a79b
Gerwin Klein <gerwin.klein@data61.csiro.au>
2019-10-14 11:31:51 +1100

    riscv crefine: set up CBaseRefine
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ proof/crefine/RISCV64/Include_C.thy
@@ -0,0 +1,17 @@
+(*
+ * Copyright 2019, Data61, CSIRO
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(DATA61_GPL)
+ *)
+
+theory Include_C
+imports
+  "CSpec.KernelInc_C"
+  "Refine.Refine"
+begin
+
+end
