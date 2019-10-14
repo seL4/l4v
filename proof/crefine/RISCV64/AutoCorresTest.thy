3f80b582eebea29142310cf3e966a090649ef83b
Gerwin Klein <gerwin.klein@data61.csiro.au>
2019-10-14 13:47:38 +1100

    riscv crefine: add AutoCorresTest stub (empty)
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ proof/crefine/RISCV64/AutoCorresTest.thy
@@ -0,0 +1,17 @@
+(*
+ * Copyright 2018, General Dynamics C4 Systems
+ *
+ * This software may be distributed and modified according to the terms of
+ * the GNU General Public License version 2. Note that NO WARRANTY is provided.
+ * See "LICENSE_GPLv2.txt" for details.
+ *
+ * @TAG(GD_GPL)
+ *)
+
+theory AutoCorresTest
+imports Refine_C
+begin
+
+(* empty stub, currently tested for ARM and ARM_HYP only *)
+
+end
