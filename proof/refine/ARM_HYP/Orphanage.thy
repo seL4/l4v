479f98de674713634e37ff4f98741896a3ba2eea
Gerwin Klein <gerwin.klein@data61.csiro.au>
2020-07-02 11:29:09 +0800

    arm_hyp refine: add an Orphanage place holder
    This makes sure Isabelle doesn't complain about a missing dependency in
    the ROOT file when ARM_HYP is selected. The complaint only shows up in
    jedit, and doesn't stop anything, but it's still nicer without.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@data61.csiro.au>

---
+++ proof/refine/ARM_HYP/Orphanage.thy
@@ -0,0 +1,13 @@
+(*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory Orphanage
+imports Refine.Refine
+begin
+
+(* FIXME: palce holder, Orphanage not proved for ARM_HYP *)
+
+end
