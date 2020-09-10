a9503cf9dd9414252f6f6e2c47a3ebe358256231
Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>
2020-09-11 08:45:38 +1000

    riscv crefine: setup proofs for clz and ctz
    Progress towards verification of new and more efficient implementations
    of library functions to could leading and trailing zeros.
    
    Signed-off-by: Matthew Brecknell <Matthew.Brecknell@data61.csiro.au>

--- proof/crefine/RISCV64/Machine_C.thy
+++ proof/crefine/RISCV64/Machine_C.thy
@@ -130,6 +130,157 @@ lemma dmo_if:
   "(doMachineOp (if a then b else c)) = (if a then (doMachineOp b) else (doMachineOp c))"
   by (simp split: if_split)
 
-end
+(* Count leading and trailing zeros. *)
 
+(* FIXME: move *)
+lemma word_ctz_max:
+  "word_ctz w \<le> size w"
+  sorry
+
+(* FIXME: move *)
+lemma hoarep_Seq_nothrow:
+  assumes c: "hoarep \<Gamma> \<Theta> F P c Q {}"
+  assumes d: "hoarep \<Gamma> \<Theta> F Q d R A"
+  shows "hoarep \<Gamma> \<Theta> F P (Seq c d) R A"
+  by (rule hoarep.Seq[OF HoarePartialDef.conseqPrePost[OF c] d]; simp)
+
+definition clz32_step where
+  "clz32_step i \<equiv> \<acute>mask___unsigned :== \<acute>mask___unsigned >> unat ((1::32 sword) << unat i);;
+                 \<acute>bits :== SCAST(32 signed \<rightarrow> 32) (if \<acute>mask___unsigned < \<acute>x then 1 else 0) << unat i;;
+                 Guard ShiftError \<lbrace>\<acute>bits < SCAST(32 signed \<rightarrow> 32) 0x20\<rbrace> (\<acute>x :== \<acute>x >> unat \<acute>bits);;
+                 \<acute>count :== \<acute>count - \<acute>bits"
+
+(* FIXME: figure out what this should be *)
+definition clz32_invariant where
+  "clz32_invariant i s \<equiv> {s'. undefined ''clz32_invariant'' i s s'}"
+
+lemma clz32_step:
+  "\<Gamma> \<turnstile> (clz32_invariant (i+1) s) clz32_step i (clz32_invariant i s)"
+  unfolding clz32_step_def
+  apply (vcg, clarsimp simp: clz32_invariant_def)
+  sorry
+
+lemma clz32_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {s} Call clz32_'proc \<lbrace>\<acute>ret__unsigned = of_nat (word_clz (x___unsigned_' s))\<rbrace>"
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply (hoarep_rewrite, fold clz32_step_def)
+  apply (intro allI hoarep.Catch[OF _ hoarep.Skip])
+  apply (rule_tac Q="clz32_invariant 0 s" in hoarep_Seq_nothrow[OF _ creturn_wp])
+  apply (rule HoarePartial.SeqSwap[OF clz32_step], simp)+
+  apply (rule conseqPre, vcg)
+  apply (all \<open>clarsimp simp: clz32_invariant_def\<close>)
+  sorry
+
+definition clz64_step where
+  "clz64_step i \<equiv> \<acute>mask___unsigned_longlong :== \<acute>mask___unsigned_longlong >> unat ((1::32 sword) << unat i);;
+                   \<acute>bits :== SCAST(32 signed \<rightarrow> 32) (if \<acute>mask___unsigned_longlong < \<acute>x___unsigned_longlong then 1 else 0) << unat i;;
+                   Guard ShiftError \<lbrace>\<acute>bits < SCAST(32 signed \<rightarrow> 32) 0x40\<rbrace> (\<acute>x___unsigned_longlong :== \<acute>x___unsigned_longlong >> unat \<acute>bits);;
+                   \<acute>count :== \<acute>count - \<acute>bits"
+
+(* FIXME: figure out what this should be *)
+definition clz64_invariant where
+  "clz64_invariant i s \<equiv> {s'. undefined ''clz64_invariant'' i s s'}"
+
+lemma clz64_step:
+  "\<Gamma> \<turnstile> (clz64_invariant (i+1) s) clz64_step i (clz64_invariant i s)"
+  unfolding clz64_step_def
+  apply (vcg, clarsimp simp: clz64_invariant_def)
+  sorry
+
+lemma clz64_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {s} Call clz64_'proc \<lbrace>\<acute>ret__unsigned = of_nat (word_clz (x___unsigned_longlong_' s))\<rbrace>"
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply (hoarep_rewrite, fold clz64_step_def)
+  apply (intro allI hoarep.Catch[OF _ hoarep.Skip])
+  apply (rule_tac Q="clz64_invariant 0 s" in hoarep_Seq_nothrow[OF _ creturn_wp])
+  apply (rule HoarePartial.SeqSwap[OF clz64_step], simp)+
+  apply (rule conseqPre, vcg)
+  apply (all \<open>clarsimp simp: clz64_invariant_def\<close>)
+  sorry
+
+definition ctz32_step where
+  "ctz32_step i \<equiv> \<acute>mask___unsigned :== \<acute>mask___unsigned >> unat ((1::32 sword) << unat i);;
+                   \<acute>bits :== SCAST(32 signed \<rightarrow> 32) (if \<acute>x && \<acute>mask___unsigned = SCAST(32 signed \<rightarrow> 32) 0 then 1 else 0) << unat i;;
+                   Guard ShiftError \<lbrace>\<acute>bits < SCAST(32 signed \<rightarrow> 32) 0x20\<rbrace> (\<acute>x :== \<acute>x >> unat \<acute>bits);;
+                   \<acute>count :== \<acute>count + \<acute>bits"
+
+(* FIXME: figure out what this should be *)
+definition ctz32_invariant where
+  "ctz32_invariant i s \<equiv> {s'. undefined ''ctz32_invariant'' i s s'}"
+
+lemma ctz32_step:
+  "\<Gamma> \<turnstile> (ctz32_invariant (i+1) s) ctz32_step i (ctz32_invariant i s)"
+  unfolding ctz32_step_def
+  apply (vcg, clarsimp simp: ctz32_invariant_def)
+  sorry
+
+lemma ctz32_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {s} Call ctz32_'proc \<lbrace>\<acute>ret__unsigned = of_nat (word_ctz (x___unsigned_' s))\<rbrace>"
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply (hoarep_rewrite, fold ctz32_step_def)
+  apply (intro allI hoarep.Catch[OF _ hoarep.Skip])
+  apply (rule_tac Q="ctz32_invariant 0 s" in hoarep_Seq_nothrow[OF _ creturn_wp])
+  apply (rule HoarePartial.SeqSwap[OF ctz32_step], simp)+
+  apply (rule conseqPre, vcg)
+  apply (all \<open>clarsimp simp: ctz32_invariant_def\<close>)
+  sorry
+
+definition ctz64_step where
+  "ctz64_step i \<equiv> \<acute>mask___unsigned_longlong :== \<acute>mask___unsigned_longlong >> unat ((1::32 sword) << unat i);;
+                   \<acute>bits :== SCAST(32 signed \<rightarrow> 32) (if \<acute>x___unsigned_longlong && \<acute>mask___unsigned_longlong = SCAST(32 signed \<rightarrow> 64) 0 then 1 else 0) << unat i;;
+                   Guard ShiftError \<lbrace>\<acute>bits < SCAST(32 signed \<rightarrow> 32) 0x40\<rbrace> (\<acute>x___unsigned_longlong :== \<acute>x___unsigned_longlong >> unat \<acute>bits);;
+                   \<acute>count :== \<acute>count + \<acute>bits"
+
+(* FIXME: figure out what this should be *)
+definition ctz64_invariant where
+  "ctz64_invariant i s \<equiv> {s'. undefined ''ctz64_invariant'' i s s'}"
+
+lemma ctz64_step:
+  "\<Gamma> \<turnstile> (ctz64_invariant (i+1) s) ctz64_step i (ctz64_invariant i s)"
+  unfolding ctz64_step_def
+  apply (vcg, clarsimp simp: ctz64_invariant_def)
+  sorry
+
+lemma ctz64_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {s} Call ctz64_'proc \<lbrace>\<acute>ret__unsigned = of_nat (word_ctz (x___unsigned_longlong_' s))\<rbrace>"
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply (hoarep_rewrite, fold ctz64_step_def)
+  apply (intro allI hoarep.Catch[OF _ hoarep.Skip])
+  apply (rule_tac Q="ctz64_invariant 0 s" in hoarep_Seq_nothrow[OF _ creturn_wp])
+  apply (rule HoarePartial.SeqSwap[OF ctz64_step], simp)+
+  apply (rule conseqPre, vcg)
+  apply (all \<open>clarsimp simp: ctz64_invariant_def\<close>)
+  sorry
+
+(* The library implementations would allow us to weaken the preconditions to allow zero inputs,
+   but we keep the stronger preconditions to preserve older proofs that use these specs. *)
+lemma clzl_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {\<sigma>. s = \<sigma> \<and> x___unsigned_long_' s \<noteq> 0}
+   Call clzl_'proc
+   \<lbrace>\<acute>ret__long = of_nat (word_clz (x___unsigned_long_' s))\<rbrace>"
+  apply (rule allI, rule conseqPre, vcg)
+  by (clarsimp simp: word_clz_max[THEN le_less_trans, THEN ucast_of_nat_small] word_size)
+
+lemma clzll_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {\<sigma>. s = \<sigma> \<and> x___unsigned_longlong_' s \<noteq> 0}
+   Call clzll_'proc
+   \<lbrace>\<acute>ret__longlong = of_nat (word_clz (x___unsigned_longlong_' s))\<rbrace>"
+  apply (rule allI, rule conseqPre, vcg)
+  by (clarsimp simp: word_clz_max[THEN le_less_trans, THEN ucast_of_nat_small] word_size)
+
+lemma ctzl_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {\<sigma>. s = \<sigma> \<and> x___unsigned_long_' s \<noteq> 0}
+   Call ctzl_'proc
+   \<lbrace>\<acute>ret__long = of_nat (word_ctz (x___unsigned_long_' s))\<rbrace>"
+  apply (rule allI, rule conseqPre, vcg)
+  by (clarsimp simp: word_ctz_max[THEN le_less_trans, THEN ucast_of_nat_small] word_size)
+
+lemma ctzll_spec:
+  "\<forall>s. \<Gamma> \<turnstile> {\<sigma>. s = \<sigma> \<and> x___unsigned_longlong_' s \<noteq> 0}
+   Call ctzll_'proc
+   \<lbrace>\<acute>ret__longlong = of_nat (word_ctz (x___unsigned_longlong_' s))\<rbrace>"
+  apply (rule allI, rule conseqPre, vcg)
+  by (clarsimp simp: word_ctz_max[THEN le_less_trans, THEN ucast_of_nat_small] word_size)
+
+end
 end
