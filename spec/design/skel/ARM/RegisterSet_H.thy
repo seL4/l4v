40abe1b28203b83826ea35e98ea625b157d4d36e
Rafal Kolanski <rafal.kolanski@data61.csiro.au>
2020-11-19 13:23:08 +1100

    arm machine+design: update for iMX8+FPU
    Update platform constants and design spec skeletons.
    
    Signed-off-by: Rafal Kolanski <rafal.kolanski@data61.csiro.au>

--- spec/design/skel/ARM/RegisterSet_H.thy
+++ spec/design/skel/ARM/RegisterSet_H.thy
@@ -13,10 +13,13 @@ imports
 begin
 context Arch begin global_naming ARM_H
 
+definition newFPUState :: "fpu_state" where
+  "newFPUState \<equiv> FPUState (K 0) 0 0 "
+
 definition
   newContext :: "user_context"
 where
- "newContext \<equiv> UserContext ((K 0) aLU initContext)"
+ "newContext \<equiv> UserContext newFPUState ((K 0) aLU initContext)"
 
 end
 end
