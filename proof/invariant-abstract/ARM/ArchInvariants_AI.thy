8ff58240e8675e22b1c796a3b243638b537f66d9
Rafal Kolanski <rafal.kolanski@data61.csiro.au>
2020-11-19 13:23:42 +1100

    arm aspec+ainvs: update for iMX8+FPU
    Signed-off-by: Rafal Kolanski <rafal.kolanski@data61.csiro.au>

--- proof/invariant-abstract/ARM/ArchInvariants_AI.thy
+++ proof/invariant-abstract/ARM/ArchInvariants_AI.thy
@@ -48,7 +48,7 @@ lemma iarch_tcb_context_set[simp]:
 lemma iarch_tcb_set_registers[simp]:
   "arch_tcb_to_iarch_tcb (arch_tcb_set_registers regs arch_tcb)
      = arch_tcb_to_iarch_tcb arch_tcb"
-  by (simp add: arch_tcb_set_registers_def)
+  by (simp add: arch_tcb_to_iarch_tcb_def)
 
 (* These simplifications allows us to keep many arch-specific proofs unchanged. *)
 
