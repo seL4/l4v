ce4ca602c4ba0b6f5643ad7a52e4e10566ac8a77
Miki Tanaka <miki.tanaka@data61.csiro.au>
2021-02-11 08:45:59 +1100

    rt design: move some option monad functions to lib
    Signed-off-by: Miki Tanaka <miki.tanaka@data61.csiro.au>

--- spec/design/skel/PSpaceStorable_H.thy
+++ spec/design/skel/PSpaceStorable_H.thy
@@ -54,22 +54,6 @@ where
 | "koTypeOf (KOKernelData) = KernelDataT"
 | "koTypeOf (KOArch e) = ArchT (archTypeOf e)"
 
-(* FIXME: move *)
-definition ounless :: "bool \<Rightarrow> unit kernel_r \<Rightarrow> unit kernel_r"
-where "ounless P f \<equiv> if P then oreturn () else f"
-
-(* FIXME: move *)
-definition owhen :: "bool \<Rightarrow> unit kernel_r \<Rightarrow> unit kernel_r"
-where "owhen P f \<equiv> if P then f else oreturn ()"
-
-(* FIXME: move *)
-definition ohaskell_fail :: "unit list \<Rightarrow> 'a kernel_r" where
-  "ohaskell_fail ls = K None"
-
-(* FIXME: move *)
-definition ohaskell_assert :: "bool \<Rightarrow> unit list \<Rightarrow> unit kernel_r" where
-  "ohaskell_assert P ls \<equiv> if P then oreturn () else ofail"
-
 definition
   read_typeError :: "unit list \<Rightarrow> kernel_object \<Rightarrow> 'a kernel_r" where
   "read_typeError t1 t2 \<equiv> ofail"
