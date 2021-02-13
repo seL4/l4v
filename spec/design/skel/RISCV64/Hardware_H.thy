151102571ab179cd05df85c5674950f489155c62
Michael McInerney <Michael.McInerney@data61.csiro.au>
2021-02-13 21:32:45 +1100

    rt design RISCV64: add maxPeriodUs
    This commit also defines maxUsToTicks to equal
    max_us_to_ticks from the abstract
    
    Signed-off-by: Michael McInerney <Michael.McInerney@data61.csiro.au>

--- spec/design/skel/RISCV64/Hardware_H.thy
+++ spec/design/skel/RISCV64/Hardware_H.thy
@@ -19,7 +19,7 @@ definition ticksToUs :: "word64 \<Rightarrow> word64" where
   "ticksToUs \<equiv> ticks_to_us"
 
 definition maxUsToTicks :: "word64" where
-  "maxUsToTicks \<equiv> 60 * 60 * 1000" (* FIXME RT: sync with aspec and C *)
+  "maxUsToTicks \<equiv> max_us_to_ticks"
 
 definition maxTicksToUs :: "word64" where
   "maxTicksToUs \<equiv> max_ticks_to_us"
@@ -30,7 +30,10 @@ definition kernelWCETTicks :: "word64" where
 definition kernelWCETUs :: "word64" where
   "kernelWCETUs \<equiv> kernelWCET_us"
 
-#INCLUDE_HASKELL SEL4/Machine/Hardware/RISCV64.hs Platform=Platform.RISCV64 CONTEXT RISCV64_H NOT plic_complete_claim getMemoryRegions getDeviceRegions getKernelDevices loadWord storeWord storeWordVM getActiveIRQ ackInterrupt maskInterrupt setDeadline configureTimer resetTimer debugPrint getRestartPC setNextPC clearMemory clearMemoryVM initMemory freeMemory setHardwareASID wordFromPDE wordFromPTE VMFaultType HypFaultType VMPageSize pageBits pageBitsForSize toPAddr addrFromPPtr ptrFromPAddr sfence physBase ptTranslationBits vmFaultTypeFSR read_sbadaddr setVSpaceRoot hwASIDFlush setIRQTrigger getCurrentTime usToTicks ticksToUs maxUsToTicks maxTicksToUs ackDeadlineIRQ
+definition maxPeriodUs :: "word64" where
+  "maxPeriodUs \<equiv> MAX_PERIOD_US"
+
+#INCLUDE_HASKELL SEL4/Machine/Hardware/RISCV64.hs Platform=Platform.RISCV64 CONTEXT RISCV64_H NOT plic_complete_claim getMemoryRegions getDeviceRegions getKernelDevices loadWord storeWord storeWordVM getActiveIRQ ackInterrupt maskInterrupt setDeadline configureTimer resetTimer debugPrint getRestartPC setNextPC clearMemory clearMemoryVM initMemory freeMemory setHardwareASID wordFromPDE wordFromPTE VMFaultType HypFaultType VMPageSize pageBits pageBitsForSize toPAddr addrFromPPtr ptrFromPAddr sfence physBase ptTranslationBits vmFaultTypeFSR read_sbadaddr setVSpaceRoot hwASIDFlush setIRQTrigger getCurrentTime usToTicks ticksToUs maxUsToTicks maxTicksToUs maxPeriodUs ackDeadlineIRQ
 
 end
 
