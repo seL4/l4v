7d77853e2d4c97de2a657e31d4b27f8f2b239a1e
Corey Lewis <Corey.Lewis@data61.csiro.au>
2021-02-26 16:07:36 +1100

    refine: remove duplicated lemmas
    Signed-off-by: Corey Lewis <Corey.Lewis@data61.csiro.au>

--- proof/refine/ARM/Syscall_R.thy
+++ proof/refine/ARM/Syscall_R.thy
@@ -1178,7 +1178,7 @@ lemma setTCB_valid_duplicates'[wp]:
  "\<lbrace>\<lambda>s. vs_valid_duplicates' (ksPSpace s)\<rbrace>
   setObject a (tcb::tcb) \<lbrace>\<lambda>rv s. vs_valid_duplicates' (ksPSpace s)\<rbrace>"
   apply (clarsimp simp: setObject_def split_def valid_def in_monad
-                        projectKOs pspace_aligned'_def ps_clear_upd'
+                        projectKOs pspace_aligned'_def ps_clear_upd
                         objBits_def[symmetric] lookupAround2_char1
                  split: if_split_asm)
   apply (frule pspace_storable_class.updateObject_type[where v = tcb,simplified])
