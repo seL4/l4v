8c8398ffbe317541ff2c286b1f25e4eae4edf299
Michael McInerney <Michael.McInerney@data61.csiro.au>
2021-03-04 23:40:19 +1100

    rt aspec: align definition of preemption_point with C
    Signed-off-by: Michael McInerney <Michael.McInerney@data61.csiro.au>
    Co-authored-by: Miki Tanaka <miki.tanaka@data61.csiro.au>

--- spec/abstract/KHeap_A.thy
+++ spec/abstract/KHeap_A.thy
@@ -624,4 +624,42 @@ definition
 throw_on_false :: "'e \<Rightarrow> (bool,'z::state_ext) s_monad \<Rightarrow> ('e + unit,'z::state_ext) s_monad" where
 "throw_on_false ex f \<equiv> doE v \<leftarrow> liftE f; unlessE v $ throwError ex odE"
 
+text \<open>Allow preemption at this point.\<close>
+
+definition
+  is_cur_domain_expired :: "'z::state_ext state \<Rightarrow> bool"
+where
+  "is_cur_domain_expired = (\<lambda>s. domain_time s < consumed_time s + MIN_BUDGET)"
+
+text \<open>Update current and consumed time.\<close>
+definition
+  update_time_stamp :: "(unit, 'z::state_ext) s_monad"
+where
+  "update_time_stamp = do
+    prev_time \<leftarrow> gets cur_time;
+    cur_time' \<leftarrow> do_machine_op getCurrentTime;
+    modify (\<lambda>s. s\<lparr> cur_time := cur_time' \<rparr>);
+    modify (\<lambda>s. s\<lparr> consumed_time := consumed_time s + cur_time' - prev_time \<rparr>)
+  od"
+
+definition
+  preemption_point :: "(unit,'z::state_ext) p_monad" where
+ "preemption_point \<equiv> doE liftE $ do_extended_op update_work_units;
+                         OR_choiceE (work_units_limit_reached)
+                           (doE liftE $ do_extended_op reset_work_units;
+                                irq_opt \<leftarrow> liftE $ do_machine_op (getActiveIRQ True);
+                                case_option (do update_time_stamp;
+                                                cur_sc \<leftarrow> gets cur_sc;
+                                                active \<leftarrow> get_sc_active cur_sc;
+                                                consumed \<leftarrow> gets consumed_time;
+                                                sufficient \<leftarrow> get_sc_refill_sufficient cur_sc consumed;
+                                                exp \<leftarrow> gets is_cur_domain_expired;
+                                                if \<not>(active \<and> sufficient) \<or> exp
+                                                then (throwError $ ())
+                                                else returnOk ()
+                                             od)
+                                            (K (throwError $ ())) irq_opt
+                            odE) (returnOk ())
+                     odE"
+
 end
