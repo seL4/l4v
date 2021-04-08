4ecf7755f3a218bc09befdbfd0ee6c0c686e9757
Miki Tanaka <miki.tanaka@data61.csiro.au>
2021-04-08 21:52:46 +1000

    arm refine: remove magic numbers from valid_irq_node' and global_refs'
    Signed-off-by: Miki Tanaka <miki.tanaka@data61.csiro.au>

--- proof/refine/ARM/Invariants_H.thy
+++ proof/refine/ARM/Invariants_H.thy
@@ -976,7 +976,8 @@ definition
   valid_irq_node' :: "word32 \<Rightarrow> kernel_state \<Rightarrow> bool"
 where
  "valid_irq_node' x \<equiv>
-  \<lambda>s. is_aligned x 12 \<and> (\<forall>irq :: irq. real_cte_at' (x + 16 * (ucast irq)) s)"
+  \<lambda>s. is_aligned x (size (0::irq) + cteSizeBits)
+      \<and> (\<forall>irq :: irq. real_cte_at' (x + (ucast irq << cteSizeBits)) s)"
 
 definition
   valid_refs' :: "word32 set \<Rightarrow> cte_heap \<Rightarrow> bool"
@@ -1000,7 +1001,7 @@ where
   {ksIdleThread s} \<union>
    page_directory_refs' (armKSGlobalPD (ksArchState s)) \<union>
    (\<Union>pt \<in> set (armKSGlobalPTs (ksArchState s)). page_table_refs' pt) \<union>
-   range (\<lambda>irq :: irq. irq_node' s + 16 * ucast irq)"
+   range (\<lambda>irq :: irq. irq_node' s + (ucast irq << cteSizeBits))"
 
 definition
   valid_cap_sizes' :: "nat \<Rightarrow> cte_heap \<Rightarrow> bool"
