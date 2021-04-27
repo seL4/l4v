5bc9a899fdc0d647a2799cccce9b45096452139d
Ryan Barry <ryan.barry@unsw.edu.au>
2021-04-27 13:35:11 +1000

    access: Finalise_AC arch split
    Signed-off-by: Ryan Barry <ryan.barry@unsw.edu.au>

---
+++ proof/access-control/ARM/ArchFinalise_AC.thy
@@ -0,0 +1,127 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchFinalise_AC
+imports Finalise_AC
+begin
+
+context Arch begin global_naming ARM_A
+
+named_theorems Finalise_AC_assms
+
+crunch pas_refined[wp]: arch_finalise_cap, prepare_thread_delete "pas_refined aag"
+
+crunch respects[Finalise_AC_assms, wp]: prepare_thread_delete "integrity aag X st"
+
+lemma sbn_st_vrefs[Finalise_AC_assms, wp]:
+  "set_bound_notification t st \<lbrace>\<lambda>s. P (state_vrefs s)\<rbrace>"
+  apply (simp add: set_bound_notification_def)
+  apply (wpsimp wp: set_object_wp dxo_wp_weak)
+  apply (clarsimp simp: state_vrefs_def vs_refs_no_global_pts_def
+                 elim!: rsubst[where P=P, OF _ ext]
+                 dest!: get_tcb_SomeD)
+  done
+
+lemma arch_finalise_cap_auth'[Finalise_AC_assms]:
+   "\<lbrace>pas_refined aag\<rbrace> arch_finalise_cap x12 final \<lbrace>\<lambda>rv s. pas_cap_cur_auth aag (fst rv)\<rbrace>"
+  unfolding arch_finalise_cap_def
+  by (wp | wpc | simp add: comp_def hoare_post_taut[where P = \<top>] split del: if_split)+
+
+lemma arch_finalise_cap_obj_refs[Finalise_AC_assms]:
+  "\<lbrace>\<lambda>s. \<forall>x \<in> aobj_ref' acap. P x\<rbrace>
+   arch_finalise_cap acap slot
+   \<lbrace>\<lambda>rv s. \<forall>x \<in> obj_refs (fst rv). P x\<rbrace>"
+  by (wpsimp simp: arch_finalise_cap_def)
+
+lemma arch_finalise_cap_makes_halted[Finalise_AC_assms]:
+  "\<lbrace>\<top>\<rbrace> arch_finalise_cap arch_cap ex \<lbrace>\<lambda>rv s. \<forall>t\<in>Access.obj_refs (fst rv). halted_if_tcb t s\<rbrace>"
+  apply (case_tac arch_cap, simp_all add: arch_finalise_cap_def)
+  by (wpsimp simp: valid_cap_def split: option.split bool.split)+
+
+lemma arch_cap_cleanup_wf[Finalise_AC_assms]:
+  "\<lbrakk> arch_cap_cleanup_opt acap \<noteq> NullCap; \<not> is_arch_cap (arch_cap_cleanup_opt acap) \<rbrakk>
+     \<Longrightarrow> (\<exists>irq. arch_cap_cleanup_opt acap = IRQHandlerCap irq \<and> is_subject_irq aag irq)"
+  by simp
+
+lemma arch_finalise_cap_respects[wp]:
+  "\<lbrace>integrity aag X st and invs and pas_refined aag and valid_cap (ArchObjectCap cap)
+                       and K (pas_cap_cur_auth aag (ArchObjectCap cap))\<rbrace>
+   arch_finalise_cap cap final
+   \<lbrace>\<lambda>_. integrity aag X st\<rbrace>"
+  apply (simp add: arch_finalise_cap_def)
+  apply (wpsimp wp: unmap_page_respects unmap_page_table_respects delete_asid_respects)
+  apply (auto simp: cap_auth_conferred_def arch_cap_auth_conferred_def is_page_cap_def
+                    aag_cap_auth_def pas_refined_all_auth_is_owns valid_cap_simps
+                    cap_links_asid_slot_def label_owns_asid_slot_def
+             intro: pas_refined_Control_into_is_subject_asid)
+  done
+
+end
+
+
+global_interpretation Finalise_AC_1?: Finalise_AC_1
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Finalise_AC_assms | wp finalise_cap_replaceable))
+qed
+
+
+context Arch begin global_naming ARM_A
+
+lemma cap_revoke_respects'[Finalise_AC_assms]:
+  "s \<turnstile> \<lbrace>(\<lambda>s. trp \<longrightarrow> integrity aag X st s) and K (is_subject aag (fst slot))
+                                           and pas_refined aag and einvs and simple_sched_action\<rbrace>
+       cap_revoke slot
+       \<lbrace>\<lambda>_. (\<lambda>s. trp \<longrightarrow> integrity aag X st s) and pas_refined aag\<rbrace>,
+       \<lbrace>\<lambda>_. (\<lambda>s. trp \<longrightarrow> integrity aag X st s) and pas_refined aag\<rbrace>"
+proof (induct rule: cap_revoke.induct[where ?a1.0=s])
+  case (1 slot s)
+  show ?case
+    apply (subst cap_revoke.simps)
+    apply (rule hoare_pre_spec_validE)
+     apply (wp "1.hyps")
+            apply ((wp preemption_point_inv' | simp add: integrity_subjects_def pas_refined_def)+)[1]
+           apply (wp select_ext_weak_wp cap_delete_respects cap_delete_pas_refined
+                  | simp split del: if_split | wp (once) hoare_vcg_const_imp_lift hoare_drop_imps)+
+    by (auto simp: emptyable_def descendants_of_def
+             dest: reply_slot_not_descendant
+            intro: cca_owned)
+qed
+
+lemma finalise_cap_caps_of_state_nullinv[Finalise_AC_assms]:
+  "\<lbrace>\<lambda>s. P (caps_of_state s) \<and> (\<forall>p. P (caps_of_state s(p \<mapsto> NullCap)))\<rbrace>
+   finalise_cap cap final
+   \<lbrace>\<lambda>_ s. P (caps_of_state s)\<rbrace>"
+  by (cases cap;
+      wpsimp wp: suspend_caps_of_state unbind_notification_caps_of_state
+                 unbind_notification_cte_wp_at
+                 hoare_vcg_all_lift hoare_drop_imps
+                 deleting_irq_handler_caps_of_state_nullinv
+           simp: fun_upd_def[symmetric] if_apply_def2 split_del: if_split)
+
+lemma finalise_cap_fst_ret[Finalise_AC_assms]:
+  "\<lbrace>\<lambda>_. P NullCap \<and> (\<forall>a b c. P (Zombie a b c))\<rbrace>
+   finalise_cap cap is_final
+   \<lbrace>\<lambda>rv _. P (fst rv)\<rbrace>"
+  including no_pre
+  apply (cases cap, simp_all add: arch_finalise_cap_def split del: if_split)
+  apply (wp | simp add: comp_def split del: if_split | fastforce)+
+  apply (rule hoare_pre)
+  apply (wp | simp | (rule hoare_pre, wpc))+
+  done
+
+end
+
+
+global_interpretation Finalise_AC_2?: Finalise_AC_2
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; fact Finalise_AC_assms)
+qed
+
+end
