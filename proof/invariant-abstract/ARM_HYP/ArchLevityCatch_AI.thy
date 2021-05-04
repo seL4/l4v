51ac27ad10c300cbc6654ce3cdc10275c74ad02a
Mitchell Buckley <Mitchell.Buckley@data61.csiro.au>
2021-05-04 15:14:50 +1000

    Some improvement to rec_del_termination
    * Add comments into proof.
    * Unwind some automation to clarify how each subgoal is resolved.
    * Remove some "in monad" lemmas about `premption_point`.
    
    Signed-off-by: Mitchell Buckley <Mitchell.Buckley@data61.csiro.au>

--- proof/invariant-abstract/ARM_HYP/ArchLevityCatch_AI.thy
+++ proof/invariant-abstract/ARM_HYP/ArchLevityCatch_AI.thy
@@ -55,18 +55,6 @@ lemma asid_high_bits_of_add:
               simp: asid_low_bits_def word_bits_def nth_ucast)
   done
 
-lemma preemption_point_success [simp,intro]:
-  "((Inr (), s') \<in> fst (preemption_point s)) \<Longrightarrow>
-  \<exists>f es. s' = s \<lparr> machine_state := machine_state s \<lparr> irq_state := f (irq_state (machine_state s)) \<rparr>, exst := es \<rparr>"
-  apply (auto simp: in_monad preemption_point_def do_machine_op_def
-                    select_f_def select_def getActiveIRQ_def alternative_def
-                    do_extended_op_def OR_choiceE_def mk_ef_def
-             split: option.splits if_splits
-             intro: exI[where x=id])
-      apply (rule_tac x=Suc in exI, rule_tac x="exst bb" in exI, force)+
-    apply (rule_tac x=id in exI, rule_tac x="exst b" in exI, force)+
-    done
-
 lemma pageBits_less_word_bits [simp]:
   "pageBits < word_bits" by (simp add: pageBits_def word_bits_conv)
 
