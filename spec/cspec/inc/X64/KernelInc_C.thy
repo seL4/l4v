f8c81b53ee6a21f5b6eaa06916051420a3570bc2
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-09-02 19:51:41 +1000

    arm_hyp cspec: accommodate new bitfield file
    This is an update for seL4/seL4#378, which amongst other things factors
    out the definition of virq into its own file.
    
    This change is only visible on ARM_HYP, and the new file does not exist
    for the other architectures, so we need to make KernelInc_C
    arch-dependent, which needs to happen in its own directory. Since the
    directory of the new file will also not exist on other architectures,
    but the ROOT file must mention it, we forcefully create that directory
    in the build Makefile.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ spec/cspec/inc/X64/KernelInc_C.thy
@@ -0,0 +1,16 @@
+(*
+ * Copyright 2014, General Dynamics C4 Systems
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory KernelInc_C
+imports
+  "Substitute"
+  "structures_defs"
+  "structures_proofs"
+  "shared_types_defs"
+  "shared_types_proofs"
+begin
+
+end
