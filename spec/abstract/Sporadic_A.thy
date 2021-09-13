ca6c6a922cf7483a28d1dfd6f34bc6671eb7da82
Miki Tanaka <miki.tanaka@unsw.edu.au>
2021-09-13 22:30:15 +1000

    rt abstract: add refill_unblock_check call alias
    - add if_cond_refill_unblock_check
    - this takes an option objref, and two option bool parameters
      "if_cond_refill_unblock_check scopt act ast"
    - the two option bool values represent the conditions for calling
      refill_unblock_check
          act = None       -> constant_bandwidth
                Some True  -> sporadic & active
                Some False -> sporadic
                              (currently also tests active, hence identical
                               to the above case, until the implication
                               "sporadic --> active" is proved)
          ast = None       -> no test or assert on cur_sc
                Some True  -> asserts that the sc pointer is not cur_sc
                              (currently replaced by test, i.e.,
                               identical to the next case)
                Some False -> tests if the sc pointer is not cur_sc
    
    Signed-off-by: Miki Tanaka <miki.tanaka@unsw.edu.au>

--- spec/abstract/Sporadic_A.thy
+++ spec/abstract/Sporadic_A.thy
@@ -1,16 +1,18 @@
 (*
  * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2021, UNSW (ABN 57 195 873 197)
  *
  * SPDX-License-Identifier: GPL-2.0-only
  *)
 
-chapter "Scheduling Contexts and Control"
+chapter "Refill Unblock Check and Sporadic Guards"
 
 theory Sporadic_A
 imports CSpaceAcc_A
 begin
 
-text \<open> This theory contains operations on scheduling contexts and scheduling control. \<close>
+text \<open> This theory contains the definition of refill unblock check function and its components,
+       and functions that combine various guards and a call to refill unblock check \<close>
 
 definition
   is_round_robin :: "obj_ref \<Rightarrow> (bool,'z::state_ext) s_monad"
@@ -78,5 +80,60 @@ where
       refill_head_overlapping_loop sc_ptr
     od
   od"
+
+text \<open>The function defined below encodes several patterns of conditions
+      for calling @{term refill_unblock_check}.
+
+      - this takes an option @{typ obj_ref}, and two @{typ "bool option"} parameters @{term act} and @{term ast}
+      - act and ast encode the conditions for calling @{term refill_unblock_check} as follows:
+
+            act   None       constant\_bandwidth
+                  Some True    sporadic \& active
+                  Some False   sporadic
+                                (currently also tests active, hence identical
+                                 to the Some True case above, until the implication
+                                 "sporadic implies active" is proved)
+            ast   None         no test or assert on cur\_sc
+                  Some True    asserts that the sc pointer is not cur\_sc
+                                (currently replaced by test, i.e.,
+                                 identical to the Some False case below)
+                  Some False   tests if the sc pointer is not cur\_sc\<close>
+
+definition if_cond_refill_unblock_check where
+  "if_cond_refill_unblock_check sc_opt active asst \<equiv>
+    maybeM (\<lambda>scp. do
+      sc \<leftarrow> get_sched_context scp;
+      cur_sc_ptr \<leftarrow> gets cur_sc;
+      guard \<leftarrow> return (case active of
+                         None \<Rightarrow> (\<not> sc_sporadic sc)
+                       | Some True \<Rightarrow> sc_sporadic sc \<and> sc_active sc
+                       | Some False \<Rightarrow> sc_sporadic sc \<and> sc_active sc);
+      when (guard \<and> (asst = Some False \<longrightarrow> scp \<noteq> cur_sc_ptr)) $
+        when (asst = Some True \<longrightarrow> scp \<noteq> cur_sc_ptr) $ refill_unblock_check scp
+    od) sc_opt"
+
+abbreviation "if_sporadic_cur_sc_assert_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt (Some False) (Some True)"
+
+abbreviation "if_sporadic_and_active_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt (Some True) None"
+
+abbreviation "if_sporadic_cur_sc_test_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt (Some False) (Some False)"
+
+abbreviation "if_sporadic_active_cur_sc_test_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt (Some True) (Some False)"
+
+abbreviation "if_sporadic_active_cur_sc_assert_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt (Some True) (Some True)"
+
+abbreviation "if_constant_bandwidth_refill_unblock_check sc_opt \<equiv>
+                  if_cond_refill_unblock_check sc_opt None None"
+
+(* check *)
+thm if_cond_refill_unblock_check_def[of _ "Some False" "Some True", simplified]
+thm if_cond_refill_unblock_check_def[of _ "Some True" "Some True", simplified]
+thm if_cond_refill_unblock_check_def[of _ "Some False" "Some False", simplified]
+thm if_cond_refill_unblock_check_def[of _ "Some True" "Some False", simplified]
 
 end
