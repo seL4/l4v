a0bf14024e2160cf68f0e92a49e7c55751cbec31
Gerwin Klein <gerwin.klein@proofcraft.systems>
2021-11-16 15:52:13 +1100

    isabelle2021-1 word_lib: sync from AFP
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- lib/Word_Lib/Word_EqI.thy
+++ lib/Word_Lib/Word_EqI.thy
@@ -19,23 +19,31 @@ text \<open>
   and expanding into an explicit list of bits.
 \<close>
 
+lemmas le_mask_high_bits_len = le_mask_high_bits[unfolded word_size]
+lemmas neg_mask_le_high_bits_len = neg_mask_le_high_bits[unfolded word_size]
+
 named_theorems word_eqI_simps
 
 lemmas [word_eqI_simps] =
-  bit_simps
   word_or_zero
   neg_mask_test_bit
   nth_ucast
   less_2p_is_upper_bits_unset
-  le_mask_high_bits
+  le_mask_high_bits_len
+  neg_mask_le_high_bits_len
   bang_eq
   is_up
   is_down
   is_aligned_nth
-  neg_mask_le_high_bits
-  word_size
+  word_size (* keep this, because the goal may contain "n < size x" terms *)
 
-lemmas word_eqI_rule = word_eqI [rule_format]
+lemmas word_eqI_folds [symmetric] =
+  push_bit_eq_mult
+  drop_bit_eq_div
+  take_bit_eq_mod
+
+(* bit_eqI subsumes the first rule; kept for backwards compatibility *)
+lemmas word_eqI_rules = word_eqI [rule_format, unfolded word_size] bit_eqI
 
 lemma test_bit_lenD:
   "bit x n \<Longrightarrow> n < LENGTH('a) \<and> bit x n" for x :: "'a :: len word"
@@ -43,26 +51,28 @@ lemma test_bit_lenD:
 
 method word_eqI uses simp simp_del split split_del cong flip =
   ((* reduce conclusion to test_bit: *)
-   rule word_eqI_rule,
+   rule word_eqI_rules,
+   (* fold common patterns into bit form *)
+   (simp only: word_eqI_folds)?,
    (* make sure we're in clarsimp normal form: *)
    (clarsimp simp: simp simp del: simp_del simp flip: flip split: split split del: split_del cong: cong)?,
    (* turn x < 2^n assumptions into mask equations: *)
    ((drule less_mask_eq)+)?,
    (* expand and distribute test_bit everywhere: *)
-   (clarsimp simp: word_eqI_simps simp simp del: simp_del simp flip: flip
+   (clarsimp simp: bit_simps word_eqI_simps simp not_less not_le simp del: simp_del simp flip: flip
              split: split split del: split_del cong: cong)?,
    (* add any additional word size constraints to new indices: *)
    ((drule test_bit_lenD)+)?,
    (* try to make progress (can't use +, would loop): *)
-   (clarsimp simp: word_eqI_simps simp simp del: simp_del simp flip: flip
+   (clarsimp simp: bit_simps word_eqI_simps simp simp del: simp_del simp flip: flip
              split: split split del: split_del cong: cong)?,
    (* helps sometimes, rarely: *)
    (simp add: simp test_bit_conj_lt del: simp_del flip: flip split: split split del: split_del cong: cong)?)
 
-method word_eqI_solve uses simp simp_del split split_del cong flip =
+method word_eqI_solve uses simp simp_del split split_del cong flip dest =
   solves \<open>word_eqI simp: simp simp_del: simp_del split: split split_del: split_del
                    cong: cong simp flip: flip;
-          (fastforce dest: test_bit_size simp: word_eqI_simps simp flip: flip
+          (fastforce dest: dest simp: bit_simps word_eqI_simps simp flip: flip
                      simp: simp simp del: simp_del split: split split del: split_del cong: cong)?\<close>
 
 end
