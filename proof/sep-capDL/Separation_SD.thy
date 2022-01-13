be04eb0b509167bdc2e498f8f21e5cf358c7f631
Gerwin Klein <gerwin.klein@proofcraft.systems>
2022-01-14 09:50:46 +1100

    isabelle2021-1: SepDSpec
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- proof/sep-capDL/Separation_SD.thy
+++ proof/sep-capDL/Separation_SD.thy
@@ -296,8 +296,6 @@ lemma offset_slot':
   "\<lbrakk>slot < 2^radix\<rbrakk> \<Longrightarrow> offset slot radix = unat slot"
   by (clarsimp simp: offset_def Word.less_mask_eq)
 
-lemmas word_bits_def = Word_32.word_bits_def
-
 lemma offset_slot:
   "\<lbrakk>slot < 2^radix; radix < word_bits\<rbrakk> \<Longrightarrow> offset (of_nat slot) radix = slot"
   apply (clarsimp simp: offset_def)
