7a914e897afaf0bc7c5825360b872e3c361c7279
Gerwin Klein <gerwin.klein@proofcraft.systems>
2022-03-17 18:20:49 +1100

    aarch64 aspec: set up Init_A
    Minor style update; set up global user page table and example kernel
    vspace uses that should satisfy invariants.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- spec/abstract/AARCH64/Init_A.thy
+++ spec/abstract/AARCH64/Init_A.thy
@@ -1,11 +1,9 @@
 (*
+ * Copyright 2022, Proofcraft Pty Ltd
  * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
  *
  * SPDX-License-Identifier: GPL-2.0-only
  *)
-
-(* FIXME AARCH64: verbatim setup copy of RISCV64; needs adjustment and validation;
-                  only minimal type-check changes performed so far if any *)
 
 chapter "An Initial Kernel State"
 
@@ -15,21 +13,19 @@ imports
   "Lib.SplitRule"
 begin
 
-context Arch begin global_naming RISCV64_A
+context Arch begin global_naming AARCH64_A
 
 text \<open>
   This is not a specification of true kernel initialisation. This theory describes a dummy
   initial state only, to show that the invariants and refinement relation are consistent.
 \<close>
 
-(* Some address sufficiently aligned address for one page *)
-definition riscv_global_pt_ptr :: obj_ref
-  where
-  "riscv_global_pt_ptr = pptr_base + 0x2000"
+(* Some address sufficiently aligned for one page *)
+definition arm_global_pt_ptr :: obj_ref where
+  "arm_global_pt_ptr = pptr_base + 0x2000"
 
 (* Sufficiently aligned for irq type + cte_level_bits *)
-definition init_irq_node_ptr :: obj_ref
-  where
+definition init_irq_node_ptr :: obj_ref where
   "init_irq_node_ptr = pptr_base + 0x3000"
 
 (* The highest user-level virtual address that is still canonical.
@@ -38,48 +34,35 @@ definition init_irq_node_ptr :: obj_ref
 definition canonical_user :: "vspace_ref" where
   "canonical_user \<equiv> mask canonical_bit"
 
-(* Kernel and ELF window are constructed so that they can be covered with one max_pt_level entry
-   each. This is not the layout the real kernel uses, but we are only trying to show that
-   the invariants are consistent. *)
-(* FIXME AARCH64: TODO
-definition init_vspace_uses :: "vspace_ref \<Rightarrow> riscvvspace_region_use"
-  where
+(* This is not the layout the real kernel uses, but we are only trying to show that
+   the invariants are consistent. These apply to the mappings of the (separate) kernel-level
+   page table in hyp mode, not the user-level page tables, which have no kernel mappings. *)
+definition init_vspace_uses :: "vspace_ref \<Rightarrow> arm_vspace_region_use" where
   "init_vspace_uses p \<equiv>
-     if p \<in> {pptr_base ..< pptr_base + (1 << 30)} then RISCVVSpaceKernelWindow
-     else if p \<in> {kernel_elf_base ..< kernel_elf_base + (1 << 20)} then RISCVVSpaceKernelELFWindow
-     else if p \<le> canonical_user then RISCVVSpaceUserRegion
-     else RISCVVSpaceInvalidRegion"
-*)
+     if p \<in> {pptr_base ..< pptr_base + (1 << 30)} then ArmVSpaceKernelWindow
+     else if p \<in> {kernel_elf_base ..< kernel_elf_base + (1 << 20)} then ArmVSpaceKernelELFWindow
+     else if p \<le> canonical_user then ArmVSpaceUserRegion
+     else ArmVSpaceInvalidRegion"
 
 
-definition init_arch_state :: arch_state
-  where
+definition init_arch_state :: arch_state where
   "init_arch_state \<equiv> \<lparr>
      arm_asid_table = Map.empty,
      arm_vmid_table = Map.empty,
      arm_next_vmid = 0,
-     arm_us_global_vspace = undefined,
-     arm_kernel_vspace = undefined,
+     arm_us_global_vspace = arm_global_pt_ptr,
+     arm_kernel_vspace = init_vspace_uses,
      arm_current_vcpu = None,
      arm_gicvcpu_numlistregs = undefined
    \<rparr>"
 
 
-(* {pptr_base ..< pptr_base + (1 << 30)} is pt index 0x100 at max_pt_level,
-   {kernel_elf_base ..< kernel_elf_base + (1 << 20)} comes out to pt index 0x1FE.
-   The rest is constructed such that the translation lines up with what the invariants want. *)
-definition global_pte :: "pt_index \<Rightarrow> pte"
-  where
-  "global_pte idx \<equiv>
-     if idx = 0x100
-     then PagePTE ((ucast (idx && mask (ptTranslationBits False - 1)) << ptTranslationBits False * 2))
-                  True {} vm_kernel_only
-     else if idx = 0x1FE
-     then PagePTE (2 << ptTranslationBits False * 2) True {} vm_kernel_only
-     else InvalidPTE"
+(* The user-level global table in hyp mode is entirely empty.
+   Kernel-level mappings are in a separate kernel page table, which is not modeled here. *)
+definition global_pte :: "pt_index \<Rightarrow> pte" where
+  "global_pte idx \<equiv> InvalidPTE"
 
-definition init_kheap :: kheap
-  where
+definition init_kheap :: kheap where
   "init_kheap \<equiv>
     (\<lambda>x. if \<exists>irq :: irq. init_irq_node_ptr + (ucast irq << cte_level_bits) = x
            then Some (CNode 0 (empty_cnode 0))
@@ -101,15 +84,13 @@ definition init_kheap :: kheap
          \<rparr>
     )"
 
-definition init_cdt :: cdt
-  where
+definition init_cdt :: cdt where
   "init_cdt \<equiv> Map.empty"
 
-definition init_ioc :: "cslot_ptr \<Rightarrow> bool"
-  where
+definition init_ioc :: "cslot_ptr \<Rightarrow> bool" where
   "init_ioc \<equiv>
-   \<lambda>(a,b). (\<exists>obj. init_kheap a = Some obj \<and>
-                  (\<exists>cap. cap_of obj b = Some cap \<and> cap \<noteq> cap.NullCap))"
+     \<lambda>(a,b). (\<exists>obj. init_kheap a = Some obj \<and>
+                    (\<exists>cap. cap_of obj b = Some cap \<and> cap \<noteq> cap.NullCap))"
 
 definition init_A_st :: "'z::state_ext state"
   where
