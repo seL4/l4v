c712f52a59e7d64f3b0fd56614626bc90a9528a4
Robert Sison <robert.sison@unimelb.edu.au>
2022-05-30 17:55:05 +1000

    timeprot-subset-invs: Clarify all we need new is for labels to be owned
    Signed-off-by: Robert Sison <robert.sison@unimelb.edu.au>

--- proof/infoflow/timeprotection/CachePartitionIntegrity.thy
+++ proof/infoflow/timeprotection/CachePartitionIntegrity.thy
@@ -19,6 +19,10 @@ definition obj_v_footprint :: "'a state \<Rightarrow> obj_ref \<Rightarrow> mach
 definition policy_owned_objs :: "'a PAS \<Rightarrow> domain \<Rightarrow> obj_ref set" where
   "policy_owned_objs aag d \<equiv> {p. pasObjectAbs aag p \<in> pasDomainAbs aag d}"
 
+(* XXX: Is this feasible? If not, is there some rephrasing of it that's feasible? *)
+definition all_labels_are_owned :: "'a PAS \<Rightarrow> bool" where
+  "all_labels_are_owned aag \<equiv> \<forall>l. \<exists>d. l \<in> pasDomainAbs aag d"
+
 definition policy_accessible_labels :: "'a PAS \<Rightarrow> domain \<Rightarrow> 'a set" where
   "policy_accessible_labels aag d \<equiv> pasDomainAbs aag d \<union>
      \<Union> {ls. \<exists>l \<in> pasDomainAbs aag d.
@@ -107,74 +111,56 @@ definition accessible_objs_well_partitioned :: "'a PAS \<Rightarrow> det_state \
 \<comment> \<open>If every domain's objects is confined to its partition, then all accessible objects, due to
   belonging to accessible domains, are confined to the union of those domains' partitions. -robs\<close>
 lemma partitioning_owned_partitions_accessible:
-  (* no_empty_domains aag \<Longrightarrow> *)
-  (* pas_domains_distinct aag \<Longrightarrow> Apparently we might not even need this either? *)
-  "owned_objs_well_partitioned aag s \<Longrightarrow> accessible_objs_well_partitioned aag s"
+  "all_labels_are_owned aag \<Longrightarrow>
+   owned_objs_well_partitioned aag s \<Longrightarrow> accessible_objs_well_partitioned aag s"
   unfolding accessible_objs_well_partitioned_def
   apply(clarsimp simp:p_footprint_def)
   apply(clarsimp simp:v_to_p_kernel_nonelf_def)
-  (* For each accessible object, call it 'y' *)
+  apply(rename_tac pa y va)
+  \<comment> \<open>We consider each accessible object, call it \<open>y\<close>.\<close>
   (* Things that matter for 'y' to be a valid object reference:
-     - It is allocated, i.e. kheap s y \<noteq> None
+     - It is allocated, i.e. kheap s y \<noteq> None.
+       This follows from the definition of obj_v_footprint.
      - Everything in its object footprint is in the kernel window, i.e.
        \<forall>va \<in> obj_v_footprint s y.
-         riscv_kernel_vspace (arch_state s) va = RISCVVSpaceKernelWindow *)
-  apply(subgoal_tac "kheap s y \<noteq> None")
-  apply(subgoal_tac "\<forall>va \<in> obj_v_footprint s y.
-         riscv_kernel_vspace (arch_state s) va = RISCVVSpaceKernelWindow")
-  (* We know the L2 partitioning scheme assigns the physical address of 'y' to some domain.
-  define yd_addr_partition where "yd_addr_partition = paddr_L2_domain (addrFromKPPtr y)"  *)
-  (* 'y' has a label given by pasObjectAbs, which is not necessarily assigned to any domain.
-  define yl where "yl = pasObjectAbs aag y"  *)
-
-  (* Attempt 1: Plug in the domain determined by the partitioning scheme.
-  apply(erule_tac x="paddr_L2_domain (addrFromKPPtr y)" in allE)
-  apply clarsimp
-  apply(clarsimp simp:policy_accessible_partitions_def policy_accessible_domains_def)
-  unfolding owned_objs_well_partitioned_def
-  apply(rule_tac x="domain_L2_partition (paddr_L2_domain (addrFromKPPtr y))" in exI)
-  apply(erule_tac x="paddr_L2_domain (addrFromKPPtr y)" in allE)
-  apply(rule conjI)
-   apply(rule_tac x="paddr_L2_domain (addrFromKPPtr y)" in exI)
-  *)
-
-  (* Attempt 2: Plug in the domain determined by the domain assignment of y's label. *)
-  (* First, assume there is one. *)
-  apply(subgoal_tac "\<exists>yd. pasDomainAbs aag yd = {pasObjectAbs aag y}")
-  apply clarsimp
-  (* Note: pas_domains_distinct not even needed?
-  apply(erule_tac x=yd in allE)
-  apply clarsimp *)
+         riscv_kernel_vspace (arch_state s) va = RISCVVSpaceKernelWindow
+       This follows from the definition of v_to_p_kernel_nonelf_opt_def. *)
+  \<comment> \<open>We know the L2 partitioning scheme assigns the physical address of \<open>y\<close> to some domain
+        \<open>paddr_L2_domain (addrFromKPPtr y)\<close>
+      But instead of that, we use the domain determined by the domain assignment
+      (via @{term\<open>pasDomainAbs\<close>}) of y's label (given by @{term\<open>pasObjectAbs\<close>}),
+      which @{term\<open>all_labels_are_owned\<close>} tells us exists.\<close>
+  apply(prop_tac "\<exists>yd. pasObjectAbs aag y \<in> pasDomainAbs aag yd")
+   apply(force simp:all_labels_are_owned_def)
   apply(clarsimp simp:policy_accessible_partitions_def policy_accessible_domains_def)
   apply(rule_tac x="domain_L2_partition yd" in exI)
   apply(rule conjI)
    apply(rule_tac x=yd in exI)
    apply clarsimp
-   (* Note: From here we're trying to get something about policy_owned_objs to tell us
-      its intersection with accessible_objs is nonempty. *)
-   (* Note: `y \<in> policy_accessible_objs aag (cur_domain s)` is what should be telling us
-      what we need to know. So I should try to say that the physical address of `y`
-      is the element that is in the (non-empty) intersection of the two sets. *)
+   \<comment> \<open>Note: \<open>y \<in> policy_accessible_objs aag (cur_domain s)\<close> is what tells
+       the intersection of the two sets is nonempty.\<close>
    apply(erule_tac x=y in in_empty_interE)
     apply(force simp:policy_owned_objs_def)
    apply force
-  apply(rename_tac pa y va yobj yd)
-  (* Reminder: pa is the physical translation of some va in y's obj_v_footprint *)
+  apply(rename_tac pa y va yd)
+  \<comment> \<open>Reminder: \<open>pa\<close> is the physical translation of some \<open>va\<close> in \<open>y\<close>'s @{term\<open>obj_v_footprint\<close>}.\<close>
   apply(clarsimp simp:v_to_p_kernel_nonelf_def v_to_p_kernel_nonelf_opt_def
     split:riscvvspace_region_use.splits)
-  (* We now use owned_objs_well_partitioned to tell us that y is in its domain's partition *)
+  \<comment> \<open>We now use @{term\<open>owned_objs_well_partitioned\<close>} to tell us \<open>y\<close> is in its domain's partition.\<close>
   unfolding owned_objs_well_partitioned_def
   apply(erule_tac x=yd in allE)
-  apply(subgoal_tac "addrFromKPPtr va \<in> p_footprint s (\<Union> (obj_v_footprint s ` policy_owned_objs aag yd))")
+  apply(subgoal_tac
+    "addrFromKPPtr va \<in> p_footprint s (\<Union> (obj_v_footprint s ` policy_owned_objs aag yd))")
    apply force
-  (* va should be in the footprint of policy_owned_objs of yd because that should include y *)
+  \<comment> \<open>Finally, \<open>va\<close> should be in the footprint of the @{term\<open>policy_owned_objs\<close>} of \<open>yd\<close>
+      because that should include \<open>y\<close>.\<close>
   apply(clarsimp simp:obj_v_footprint_def p_footprint_def v_to_p_kernel_nonelf_def policy_owned_objs_def)
   apply(rule_tac x=y in exI)
   apply clarsimp
   apply(rule_tac x=va in bexI)
    apply(force simp:v_to_p_kernel_nonelf_opt_def)
   apply force
-  sorry
+  done
 
 \<comment> \<open>Object subset property: Only the paddrs of policy-accessible objects are ever accessed.\<close>
 definition ta_subset_accessible_objects :: "'a PAS \<Rightarrow> det_state \<Rightarrow> bool" where
@@ -184,14 +170,13 @@ definition ta_subset_accessible_objects :: "'a PAS \<Rightarrow> det_state \<Rig
 
 lemma ta_subset_accessible_partitions':
   (* XXX: Needed? no_empty_domains aag \<Longrightarrow> pas_domains_distinct aag \<Longrightarrow> *)
-  "\<lbrakk>accessible_objs_well_partitioned aag s;
+  "\<lbrakk>all_labels_are_owned aag; accessible_objs_well_partitioned aag s;
     ta_subset_accessible_objects aag s\<rbrakk> \<Longrightarrow>
    ta_subset_accessible_partitions aag s"
   sorry
 
 lemma ta_subset_accessible_partitions:
-  (* XXX: Needed? no_empty_domains aag \<Longrightarrow> pas_domains_distinct aag \<Longrightarrow> *)
-  "\<lbrakk>owned_objs_well_partitioned aag s;
+  "\<lbrakk>all_labels_are_owned aag; owned_objs_well_partitioned aag s;
     ta_subset_accessible_objects aag s\<rbrakk> \<Longrightarrow>
    ta_subset_accessible_partitions aag s"
   apply(force intro:ta_subset_accessible_partitions' partitioning_owned_partitions_accessible)
