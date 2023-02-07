6fd6468a8c47d3220961ed1b638a4bd3bd3428b4
Corey Lewis <corey.lewis@proofcraft.systems>
2023-02-07 20:59:02 +1100

    aspec: name remaining ThreadControl fields
    This automatically generates matching selectors.
    
    Signed-off-by: Corey Lewis <corey.lewis@proofcraft.systems>

--- spec/abstract/Invocations_A.thy
+++ spec/abstract/Invocations_A.thy
@@ -41,7 +41,7 @@ datatype tcb_invocation =
     WriteRegisters machine_word bool "machine_word list" arch_copy_register_sets
   | ReadRegisters machine_word bool machine_word arch_copy_register_sets
   | CopyRegisters machine_word machine_word bool bool bool bool arch_copy_register_sets
-  | ThreadControl machine_word cslot_ptr
+  | ThreadControl (tc_target: machine_word) (tc_slot: cslot_ptr)
                   (tc_new_fault_ep: "cap_ref option")
                   (tc_new_mcpriority: "(word8 * obj_ref) option")
                   (tc_new_priority: "(word8 * obj_ref) option")
