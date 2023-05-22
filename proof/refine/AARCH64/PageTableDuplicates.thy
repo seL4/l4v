a4f944d094b0573a00fac1a888959b27d8dde482
Gerwin Klein <gerwin.klein@proofcraft.systems>
2023-05-22 17:52:03 +1000

    aarch64 refine: copy PageTableDuplicates from RISCV64
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ proof/refine/AARCH64/PageTableDuplicates.thy
@@ -0,0 +1,47 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory PageTableDuplicates
+imports Syscall_R
+begin
+
+context begin interpretation Arch . (*FIXME: arch_split*)
+
+lemma doMachineOp_ksPSpace_inv[wp]:
+  "\<lbrace>\<lambda>s. P (ksPSpace s)\<rbrace> doMachineOp f \<lbrace>\<lambda>ya s. P (ksPSpace s)\<rbrace>"
+  by (simp add:doMachineOp_def split_def | wp)+
+
+lemma foldr_data_map_insert[simp]:
+  "foldr (\<lambda>addr map a. if a = addr then Some b else map a) = foldr (\<lambda>addr. data_map_insert addr b)"
+  apply (rule ext)+
+  apply (simp add:data_map_insert_def[abs_def] fun_upd_def)
+  done
+
+crunch arch_inv[wp]: resetUntypedCap "\<lambda>s. P (ksArchState s)"
+  (simp: crunch_simps
+     wp: hoare_drop_imps unless_wp mapME_x_inv_wp
+         preemptionPoint_inv)
+
+lemma mapM_x_mapM_valid:
+  "\<lbrace> P \<rbrace> mapM_x f xs \<lbrace>\<lambda>r. Q\<rbrace> \<Longrightarrow> \<lbrace>P\<rbrace>mapM f xs \<lbrace>\<lambda>r. Q\<rbrace>"
+  apply (simp add: mapM_x_mapM)
+  apply (clarsimp simp:valid_def return_def bind_def)
+  apply (drule spec)
+  apply (erule impE)
+   apply simp
+  apply (drule(1) bspec)
+  apply fastforce
+  done
+
+declare withoutPreemption_lift [wp del]
+
+crunch valid_cap'[wp]:
+  isFinalCapability "\<lambda>s. valid_cap' cap s"
+  (wp: crunch_wps filterM_preserved simp: crunch_simps unless_def)
+
+end
+
+end
