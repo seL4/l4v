46ab3de9d5bb399a87f579ddc25d4ecf7579a4ea
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-01-17 10:22:00 +1100

    aarch64 haskell: pTablePartialOverlap assertion
    Necessary in CRefine for deleteObjects
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- spec/haskell/src/SEL4/Model/PSpace/AARCH64.hs
+++ spec/haskell/src/SEL4/Model/PSpace/AARCH64.hs
@@ -13,12 +13,19 @@ import Prelude hiding (Word)
 import SEL4.Model.StateData
 import SEL4.Model.StateData.AARCH64
 import SEL4.Machine.RegisterSet
+import SEL4.Machine.Hardware.AARCH64 (PT_Type)
 
 import Data.Bits
+
+-- an assertion like cNodePartialOverlap, but for page tables, defined in Refine
+pTablePartialOverlap :: (Word -> Maybe PT_Type) -> (Word -> Bool) -> Bool
+pTablePartialOverlap _ _ = False
 
 deleteGhost :: PPtr a -> Int -> Kernel ()
 deleteGhost ptr bits = do
     let inRange = (\x -> x .&. ((- mask bits) - 1) == fromPPtr ptr)
     ptTypes <- gets (gsPTTypes . ksArchState)
     let ptTypes' = (\x -> if inRange x then Nothing else ptTypes x)
+    stateAssert (\ks -> not (pTablePartialOverlap ptTypes inRange))
+        "Object deletion would split page tables"
     modify (\ks -> ks { ksArchState = (ksArchState ks) { gsPTTypes = ptTypes' } })
