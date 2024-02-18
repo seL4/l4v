368a82123644cf5ebe31b0a021221ec0e00a4134
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-02-18 15:39:46 +1100

    c-parser + autocorres: Simpl update
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- tools/c-parser/testfiles/fnptr.thy
+++ tools/c-parser/testfiles/fnptr.thy
@@ -36,74 +36,66 @@ definition
 
 lemma cvc_updates_global1: "!!x. \<Gamma> \<turnstile> \<lbrace> \<acute>global1 = x \<and> symbols_ok \<rbrace>
     \<acute>ret__int :== PROC callvoidcaller() \<lbrace> \<acute>global1 = x + 1 \<rbrace>"
-apply (hoare_rule HoarePartial.ProcNoRec1)
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply (rule creturn_wp)
- apply vcg_step
- apply vcg_step
- defer
- apply vcg_step
- apply vcg_step
- apply vcg_step
- apply vcg_step
- apply vcg_step
- apply vcg_step
- apply (rule creturn_wp)
- apply vcg_step
- apply vcg_step
- apply (rule order_refl)
-
- apply (rule hoare_indirect_call_procs_consistent,
-    rule callable1_name)
- apply vcg
-
- using callable1_name
- apply (clarsimp simp: symbols_ok_def scast_id
-    procs_consistent_safe)
-done
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply vcg_step
+   apply vcg_step
+  apply vcg_step
+    apply vcg_step
+   apply vcg_step
+  apply vcg_step
+   apply vcg_step
+  apply vcg_step
+   defer
+   apply vcg_step
+    apply vcg_step
+   apply vcg_step
+     apply vcg_step
+    apply vcg_step
+   apply vcg_step
+    apply vcg_step
+   apply vcg_step
+    apply (rule order_refl)
+   apply (rule hoare_indirect_call_procs_consistent, rule callable1_name)
+   apply vcg
+  using callable1_name
+  apply (clarsimp simp: symbols_ok_def procs_consistent_safe)
+  done
 
 lemma cic_returns_4:
   "\<Gamma>\<turnstile> {| symbols_ok |} \<acute>ret__int :== PROC callintcaller()
                         {| \<acute>ret__int = 4 |}"
-apply (hoare_rule HoarePartial.ProcNoRec1)
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply (rule creturn_wp)
-apply vcg_step
+  apply (hoare_rule HoarePartial.ProcNoRec1)
+  apply vcg_step
+   apply vcg_step
+  apply vcg_step
+    apply vcg_step
+   apply vcg_step
+  apply vcg_step
+   apply vcg_step
 
-apply (rule HoarePartial.CallBody
-           [where R = "%s t. { u. ret__int_' t = 4 }"
-               and \<Gamma>=\<Gamma>, OF _ _ _ intcaller_impl])
+  apply (rule HoarePartial.CallBody
+             [where R = "%s t. { u. ret__int_' t = 4 }"
+                 and \<Gamma>=\<Gamma>, OF _ _ _ intcaller_impl])
     defer
     apply (simp only: intcaller_body_def)
-apply (rule allI)
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply vcg_step
-apply (rule creturn_wp)
-apply vcg_step
-apply vcg_step
-apply (rule order_refl)
-apply (rule hoare_indirect_call_procs_consistent)
-apply (rule intcallable2_name)
-apply simp
-apply vcg
-apply vcg
-using intcallable2_name
-apply (auto simp: symbols_ok_def procs_consistent_safe)[1]
-done
+    apply (rule allI)
+    apply vcg_step
+     apply vcg_step
+    apply vcg_step
+      apply vcg_step
+     apply vcg_step
+    apply vcg_step
+     apply vcg_step
+    apply vcg_step
+     apply (rule order_refl)
+    apply (rule hoare_indirect_call_procs_consistent)
+     apply (rule intcallable2_name)
+    apply simp
+    apply vcg
+   apply vcg
+  using intcallable2_name
+  apply (auto simp: symbols_ok_def procs_consistent_safe)[1]
+  done
 
 end
 
