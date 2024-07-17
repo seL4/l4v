c3a2c58f7de297580a21b40664b5e8d432ca7da1
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-07-17 16:45:53 +1000

    spec: defer cache flush in retype
    Defer the cache flush done in untyped_reset (inside clearMemory) to the
    actual retype call, and only flush for the object types where that is
    needed. This affects mostly the Arm architectures, but has some minor
    changes for adapting the arch interface init_arch_objects in RISCV64 and
    X64.
    
    See seL4/seL4#1289 for more detailed rationale.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- spec/abstract/AARCH64/ArchRetype_A.thy
+++ spec/abstract/AARCH64/ArchRetype_A.thy
@@ -24,9 +24,34 @@ definition reserve_region :: "obj_ref \<Rightarrow> nat \<Rightarrow> bool \<Rig
 
 text \<open>Initialise architecture-specific objects.\<close>
 
+definition vs_apiobj_size where
+  "vs_apiobj_size ty \<equiv>
+     case ty of
+       ArchObject SmallPageObj \<Rightarrow> pageBitsForSize ARMSmallPage
+     | ArchObject LargePageObj \<Rightarrow> pageBitsForSize ARMLargePage
+     | ArchObject HugePageObj \<Rightarrow> pageBitsForSize ARMHugePage
+     | ArchObject PageTableObj \<Rightarrow> table_size NormalPT_T
+     | ArchObject VSpaceObj \<Rightarrow> table_size VSRootPT_T"
+
 definition init_arch_objects ::
-  "apiobject_type \<Rightarrow> obj_ref \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> obj_ref list \<Rightarrow> (unit,'z::state_ext) s_monad" where
-  "init_arch_objects new_type ptr num_objects obj_sz refs \<equiv> return ()"
+  "apiobject_type \<Rightarrow> bool \<Rightarrow> obj_ref \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> obj_ref list \<Rightarrow> (unit,'z::state_ext) s_monad"
+  where
+  "init_arch_objects new_type is_device ptr num_objects obj_sz refs \<equiv>
+     if \<not>is_device \<and>
+        new_type \<in> {ArchObject SmallPageObj, ArchObject LargePageObj, ArchObject HugePageObj}
+     then
+       mapM_x (\<lambda>ref. do_machine_op $
+                       cleanCacheRange_RAM ref (ref + mask (vs_apiobj_size new_type))
+                                           (addrFromPPtr ref))
+              refs
+     else if new_type \<in> {ArchObject PageTableObj, ArchObject VSpaceObj}
+     then
+       mapM_x (\<lambda>ref. do_machine_op $
+                       cleanCacheRange_PoU ref (ref + mask (vs_apiobj_size new_type))
+                                           (addrFromPPtr ref))
+              refs
+     else
+       return ()"
 
 definition empty_context :: user_context where
   "empty_context \<equiv> UserContext (FPUState (\<lambda>_. 0) 0 0) (\<lambda>_. 0)"
