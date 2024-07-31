e9aefe841ffabb50c6f81b191870bac7e55ced6a
Rob Sison <r.sison@unsw.edu.au>
2024-07-31 15:51:16 +1000

    experimental-mspec: Start defining Microkit-facing state projection
    Signed-off-by: Rob Sison <r.sison@unsw.edu.au>

---
+++ proof/invariant-abstract/MSpec_AI.thy
@@ -0,0 +1,54 @@
+(*
+ * Copyright 2024, The University of New South Wales
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory MSpec_AI
+imports
+  Syscall_AI
+begin
+
+(* From Structures_A.thy *)
+term "s :: abstract_state"
+
+(* Based on KernelState in Mathieu's gordian-relation-proof *)
+record mspec_state =
+  cur_thread_cnode :: cnode_contents
+  cur_thread_bound_notification :: "obj_ref option"
+  (* Dropped: (ks_reply_obj_has_cap (Array SeL4_Cap Bool)).
+     I think this can instead be phrased as a predicate on the CNode contents.
+     See valid_reply_obj_for_tcb below for experimentation. *)
+  (* TODO: (ks_recv_oracle (Maybe KernelOracle)) *)
+  (* TODO: (ks_local_mem Mem) *)
+  (* TODO: (ks_local_mem_writable (Array Word64 Bool)) *)
+  (* TODO: (ks_local_mem_safe (Array Word64 Bool)) *)
+
+term "TCB t"
+term "t :: tcb"
+definition
+  mspec_transform :: "abstract_state \<Rightarrow> mspec_state"
+where
+  "mspec_transform s \<equiv> \<lparr>
+     cur_thread_cnode = (case kheap s (cur_thread s) of
+       Some (TCB t) \<Rightarrow> tcb_cnode_map t |
+       _ \<Rightarrow> \<lambda>_. None),
+     cur_thread_bound_notification = (case kheap s (cur_thread s) of
+       Some (TCB t) \<Rightarrow> tcb_bound_notification t |
+       _ \<Rightarrow> None)
+   \<rparr>"
+
+term "is_reply_cap"
+definition
+  valid_reply_obj_for_tcb :: "mspec_state \<Rightarrow> abstract_state \<Rightarrow> cnode_index \<Rightarrow> obj_ref \<Rightarrow> bool"
+where
+  (* FIXME: Ideally we don't want to take the abstract_state s as an argument here,
+     if we want to phrase this purely as a predicate on mspec_state m. But that means
+     getting what we need of `kheap s` into the mspec_state - maybe as local_mem? *)
+  "valid_reply_obj_for_tcb m s i tp \<equiv> case cur_thread_cnode m i of
+     Some (ReplyCap rp _) \<Rightarrow> (case kheap s rp of
+       Some (Reply r) \<Rightarrow> reply_tcb r = Some tp |
+       _ \<Rightarrow> False) |
+     _ \<Rightarrow> False"
+
+end
