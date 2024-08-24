9c1b7001622e4615e99d2d4210aae0b218453874
Gerwin Klein <gerwin.klein@proofcraft.systems>
2024-08-24 09:57:58 +0100

    arm ainvs: make proof generic in num_domains
    Transplant corresponding proofs by Raf from master branch.
    
    Co-authored-by: Rafal Kolanski <rafal.kolanski@proofcraft.systems>
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- proof/invariant-abstract/ARM/ArchDetSchedDomainTime_AI.thy
+++ proof/invariant-abstract/ARM/ArchDetSchedDomainTime_AI.thy
@@ -56,25 +56,42 @@ crunch domain_time_inv [wp, DetSchedDomainTime_AI_assms]: arch_perform_invocatio
 crunch domain_list_inv [wp, DetSchedDomainTime_AI_assms]: arch_perform_invocation "\<lambda>s. P (domain_list s)"
   (wp: crunch_wps check_cap_inv)
 
+crunches thread_set_time_slice
+  for domain_time[wp]: "\<lambda>s. P (domain_time s)"
+
+lemma timer_tick_valid_domain_time:
+  "\<lbrace> \<lambda>s :: det_ext state. 0 < domain_time s \<rbrace>
+   timer_tick
+   \<lbrace>\<lambda>x s. domain_time s = 0 \<longrightarrow> scheduler_action s = choose_new_thread\<rbrace>" (is "\<lbrace> ?dtnot0 \<rbrace> _ \<lbrace> _ \<rbrace>")
+  unfolding timer_tick_def
+  supply if_split[split del]
+  supply ethread_get_wp[wp del]
+  supply if_apply_def2[simp]
+  apply (wpsimp
+           wp: reschedule_required_valid_domain_time  hoare_vcg_const_imp_lift gts_wp
+               (* unless we hit dec_domain_time we know ?dtnot0 holds on the state, so clean up the
+                  postcondition once we hit thread_set_time_slice *)
+               hoare_post_imp[where Q="\<lambda>_. ?dtnot0" and R="\<lambda>_ s. domain_time s = 0 \<longrightarrow> X s"
+                                and a="thread_set_time_slice t ts" for X t ts]
+               hoare_drop_imp[where f="ethread_get t f" for t f])
+  apply fastforce
+  done
+
 lemma handle_interrupt_valid_domain_time [DetSchedDomainTime_AI_assms]:
-  "\<lbrace>\<lambda>s :: det_ext state. 0 < domain_time s \<rbrace> handle_interrupt i
-   \<lbrace>\<lambda>rv s.  domain_time s = 0 \<longrightarrow> scheduler_action s = choose_new_thread \<rbrace>"
-   apply (unfold handle_interrupt_def)
-   apply (case_tac "maxIRQ < i"; simp)
-    subgoal by (wp hoare_false_imp, simp)
-   apply (rule hoare_pre)
-    apply (wp do_machine_op_exst | simp add: arch_mask_irq_signal_def | wpc)+
-       apply (rule_tac Q="\<lambda>_ s. 0 < domain_time s" in hoare_post_imp, fastforce)
-       apply wp
-      apply (rule_tac Q="\<lambda>_ s. 0 < domain_time s" in hoare_post_imp, fastforce)
-      apply wp+
-     apply simp (* dxo_eq *)
-     apply (clarsimp simp: timer_tick_def num_domains_def)
-     apply (wp reschedule_required_valid_domain_time
-           | simp add: handle_reserved_irq_def
-           | wp (once) hoare_drop_imp)+
-    apply clarsimp
-   done
+  "\<lbrace>\<lambda>s :: det_ext state. 0 < domain_time s \<rbrace>
+   handle_interrupt i
+   \<lbrace>\<lambda>rv s.  domain_time s = 0 \<longrightarrow> scheduler_action s = choose_new_thread \<rbrace>" (is "\<lbrace> ?dtnot0 \<rbrace> _ \<lbrace> _ \<rbrace>")
+  unfolding handle_interrupt_def
+  apply (case_tac "maxIRQ < i", solves \<open>wpsimp wp: hoare_false_imp\<close>)
+  apply clarsimp
+  apply (wpsimp simp: arch_mask_irq_signal_def)
+        apply (rule hoare_post_imp[where Q="\<lambda>_. ?dtnot0" and a="send_signal p c" for p c], fastforce)
+        apply wpsimp
+       apply (rule hoare_post_imp[where Q="\<lambda>_. ?dtnot0" and a="get_cap p" for p], fastforce)
+      apply (wpsimp wp: timer_tick_valid_domain_time simp: handle_reserved_irq_def)+
+     apply (rule hoare_post_imp[where Q="\<lambda>_. ?dtnot0" and a="get_irq_state i" for i], fastforce)
+   apply wpsimp+
+  done
 
 crunch domain_time_inv [wp, DetSchedDomainTime_AI_assms]: handle_reserved_irq "\<lambda>s. P (domain_time s)"
   (wp: crunch_wps mapM_wp subset_refl simp: crunch_simps)
