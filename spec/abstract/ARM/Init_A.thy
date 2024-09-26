7556d074bcd30b0a19bf4fdcd972b1bb4ff49f24
Corey Lewis <corey.lewis@proofcraft.systems>
2024-09-26 15:39:10 +1000

    arm+arm-hyp+riscv64 spec: update other arches for semi-lazy FPU changes
    These architectures do not have FPU enabled and so just need minor
    updates to match the changed interface.
    
    Signed-off-by: Corey Lewis <corey.lewis@proofcraft.systems>

--- spec/abstract/ARM/Init_A.thy
+++ spec/abstract/ARM/Init_A.thy
@@ -77,6 +77,7 @@ definition
     tcb_priority = 0,
     tcb_time_slice = timeSlice,
     tcb_domain = default_domain,
+    tcb_flags = {},
     tcb_arch = init_arch_tcb
   \<rparr>,
   init_globals_frame \<mapsto> ArchObj (DataPage False ARMSmallPage), \<comment> \<open>same reason as why we kept the definition of @{term init_globals_frame}\<close>
