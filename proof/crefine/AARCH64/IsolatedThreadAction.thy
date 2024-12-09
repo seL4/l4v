c8e334d56bf2e201d8047568d33f539ce096e446
Rafal Kolanski <rafal.kolanski@proofcraft.systems>
2024-12-09 23:31:22 +1100

    aarch64 crefine: minimal fixups for arch-split up to InvariantUpdates_H
    Includes tcb_cte_cases tweaks, and a bit of effort to minimise Arch
    spillage in Move_C.
    
    Signed-off-by: Rafal Kolanski <rafal.kolanski@proofcraft.systems>

--- proof/crefine/AARCH64/IsolatedThreadAction.thy
+++ proof/crefine/AARCH64/IsolatedThreadAction.thy
@@ -142,11 +142,11 @@ end
 
 lemmas getObject_return_tcb
     = getObject_return[OF meta_eq_to_obj_eq, OF loadObject_tcb,
-                       unfolded objBits_simps, simplified]
+                       unfolded gen_objBits_simps, simplified]
 
 lemmas setObject_modify_tcb
     = setObject_modify[OF _ meta_eq_to_obj_eq, OF _ updateObject_tcb,
-                       unfolded objBits_simps, simplified]
+                       unfolded gen_objBits_simps, simplified]
 
 lemma partial_overwrite_fun_upd:
   "inj idx \<Longrightarrow>
