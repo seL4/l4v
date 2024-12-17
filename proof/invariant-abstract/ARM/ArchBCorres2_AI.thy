0f66b75e708728cb0bf232c077cf88aca179a289
Corey Lewis <corey.lewis@proofcraft.systems>
2024-12-17 13:06:37 +1100

    arm ainvs: update for explicit fpu changes
    Signed-off-by: Corey Lewis <corey.lewis@proofcraft.systems>

--- proof/invariant-abstract/ARM/ArchBCorres2_AI.thy
+++ proof/invariant-abstract/ARM/ArchBCorres2_AI.thy
@@ -28,8 +28,9 @@ crunch invoke_untyped
   for (bcorres) bcorres[wp]: truncate_state
   (ignore: sequence_x)
 
-crunch set_mcpriority, set_priority,
-          arch_get_sanitise_register_info, arch_post_modify_registers
+crunch
+  set_mcpriority, set_priority, arch_get_sanitise_register_info, arch_post_modify_registers,
+  set_flags, arch_post_set_flags
   for (bcorres) bcorres[wp, BCorres2_AI_assms]: truncate_state
 
 lemma invoke_tcb_bcorres[wp]:
@@ -67,7 +68,7 @@ lemma make_arch_fault_msg_bcorres[wp,BCorres2_AI_assms]:
   "bcorres (make_arch_fault_msg a b) (make_arch_fault_msg a b)"
   by (cases a; simp ; wp)
 
-lemma  handle_arch_fault_reply_bcorres[wp,BCorres2_AI_assms]:
+lemma handle_arch_fault_reply_bcorres[wp,BCorres2_AI_assms]:
   "bcorres ( handle_arch_fault_reply a b c d) (handle_arch_fault_reply a b c d)"
   by (cases a; simp add: handle_arch_fault_reply_def; wp)
 
@@ -99,7 +100,7 @@ lemma decode_cnode_invocation[wp]: "bcorres (decode_cnode_invocation a b c d) (d
 crunch
   decode_set_ipc_buffer, decode_set_space, decode_set_priority,
   decode_set_mcpriority, decode_set_sched_params, decode_bind_notification,
-  decode_unbind_notification, decode_set_tls_base
+  decode_unbind_notification, decode_set_tls_base, decode_set_flags
   for (bcorres) bcorres[wp]: truncate_state
 
 lemma decode_tcb_configure_bcorres[wp]: "bcorres (decode_tcb_configure b (cap.ThreadCap c) d e)
@@ -107,7 +108,8 @@ lemma decode_tcb_configure_bcorres[wp]: "bcorres (decode_tcb_configure b (cap.Th
   apply (simp add: decode_tcb_configure_def | wp)+
   done
 
-lemma decode_tcb_invocation_bcorres[wp]:"bcorres (decode_tcb_invocation a b (cap.ThreadCap c) d e) (decode_tcb_invocation a b (cap.ThreadCap c) d e)"
+lemma decode_tcb_invocation_bcorres[wp]:
+  "bcorres (decode_tcb_invocation a b (cap.ThreadCap c) d e) (decode_tcb_invocation a b (cap.ThreadCap c) d e)"
   apply (simp add: decode_tcb_invocation_def)
   apply (wp | wpc | simp)+
   done
