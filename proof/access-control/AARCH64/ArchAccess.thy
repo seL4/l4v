967b93b3999a4342bbd95a18fb0f21c7612faf92
Ryan Barry <ryan.barry@proofcraft.systems>
2025-04-22 15:50:01 +1000

    aarch64 access: copy riscv64 theories
    Signed-off-by: Ryan Barry <ryan.barry@proofcraft.systems>

---
+++ proof/access-control/AARCH64/ArchAccess.thy
@@ -0,0 +1,168 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchAccess
+imports Types
+begin
+
+context Arch begin arch_global_naming
+
+subsection \<open>Arch-specific transformation of caps into authorities\<close>
+
+definition vspace_cap_rights_to_auth :: "cap_rights \<Rightarrow> auth set" where
+  "vspace_cap_rights_to_auth r \<equiv>
+     (if AllowWrite \<in> r then {Write} else {})
+   \<union> (if AllowRead \<in> r then {Read} else {})"
+
+definition arch_cap_auth_conferred where
+  "arch_cap_auth_conferred arch_cap \<equiv>
+     (if is_FrameCap arch_cap then vspace_cap_rights_to_auth (acap_rights arch_cap) else {Control})"
+
+subsection \<open>Generating a policy from the current ASID distribution\<close>
+
+definition pte_ref2 where
+  "pte_ref2 level pte \<equiv> case pte of
+     PagePTE ppn atts rights
+       \<Rightarrow> Some (ptrFromPAddr (addr_from_ppn ppn),
+                pageBitsForSize (vmpage_size_of_level level),
+                vspace_cap_rights_to_auth rights)
+   | PageTablePTE ppn atts
+       \<Rightarrow> Some (ptrFromPAddr (addr_from_ppn ppn), 0, {Control})
+   | _ \<Rightarrow> None"
+
+definition vs_refs_aux :: "vm_level \<Rightarrow> arch_kernel_obj \<Rightarrow> (obj_ref \<times> obj_ref \<times> aa_type \<times> auth) set"
+  where
+  "vs_refs_aux level \<equiv> \<lambda>ko. case ko of
+     ASIDPool pool \<Rightarrow> (\<lambda>(r,p). (p, ucast r, AASIDPool, Control)) ` graph_of pool
+   | PageTable pt \<Rightarrow>
+       \<Union>(r,(p, sz, auth)) \<in> graph_of (pte_ref2 level o pt) - {(x,y). x \<in> kernel_mapping_slots \<and> level = max_pt_level}.
+         (\<lambda>(p, a). (p, ucast r, APageTable, a)) ` (ptr_range p sz \<times> auth)
+   | _ \<Rightarrow> {}"
+
+definition state_vrefs where
+  "state_vrefs s \<equiv> \<lambda>p.
+     \<Union>{vs_refs_aux lvl ao | lvl ao bot asid vref. vs_lookup_table bot asid vref s = Some (lvl, p)
+                                                   \<and> aobjs_of s p = Some ao \<and> vref \<in> user_region}"
+
+primrec aobj_ref' where
+  "aobj_ref' (ASIDPoolCap p as) = {p}"
+| "aobj_ref' ASIDControlCap = {}"
+| "aobj_ref' (FrameCap ref cR sz dev as) = ptr_range ref (pageBitsForSize sz)"
+| "aobj_ref' (PageTableCap x as3) = {x}"
+
+fun acap_asid' :: "arch_cap \<Rightarrow> asid set" where
+  "acap_asid' (FrameCap _ _ _ _ mapping) = fst ` set_option mapping"
+| "acap_asid' (PageTableCap _ mapping) = fst ` set_option mapping"
+| "acap_asid' (ASIDPoolCap _ asid)
+     = {x. asid_high_bits_of x = asid_high_bits_of asid \<and> x \<noteq> 0}"
+| "acap_asid' ASIDControlCap = UNIV"
+
+inductive_set state_asids_to_policy_aux for aag caps asid_tab vrefs where
+  sata_asid:
+    "\<lbrakk> caps ptr = Some (ArchObjectCap acap); asid \<in> acap_asid' acap \<rbrakk>
+       \<Longrightarrow> (pasObjectAbs aag (fst ptr), Control, pasASIDAbs aag asid)
+             \<in> state_asids_to_policy_aux aag caps asid_tab vrefs"
+| sata_asid_lookup:
+    "\<lbrakk> asid_tab (asid_high_bits_of asid) = Some poolptr;
+       (pdptr, ucast (asid && mask asid_low_bits), AASIDPool, a) \<in> vrefs poolptr \<rbrakk>
+       \<Longrightarrow> (pasASIDAbs aag asid, a, pasObjectAbs aag pdptr)
+             \<in> state_asids_to_policy_aux aag caps asid_tab vrefs"
+| sata_asidpool:
+    "\<lbrakk> asid_tab (asid_high_bits_of asid) = Some poolptr; asid \<noteq> 0 \<rbrakk>
+       \<Longrightarrow> (pasObjectAbs aag poolptr, AAuth ASIDPoolMapsASID, pasASIDAbs aag asid)
+             \<in> state_asids_to_policy_aux aag caps asid_tab vrefs"
+
+definition
+  "state_asids_to_policy_arch aag caps astate vrefs \<equiv>
+     state_asids_to_policy_aux aag caps (riscv_asid_table astate)
+                               (vrefs :: 64 word \<Rightarrow> (64 word \<times> 64 word \<times> aa_type \<times> auth) set)"
+declare state_asids_to_policy_arch_def[simp]
+
+section \<open>Arch-specific integrity definition\<close>
+
+subsection \<open>How ASIDs can change\<close>
+
+definition integrity_asids_2 ::
+  "'a PAS \<Rightarrow> 'a set \<Rightarrow> obj_ref \<Rightarrow> asid \<Rightarrow> arch_state \<Rightarrow> arch_state \<Rightarrow>
+   (obj_ref \<rightharpoonup> arch_kernel_obj) \<Rightarrow> (obj_ref \<rightharpoonup> arch_kernel_obj) \<Rightarrow> bool"
+where
+  "integrity_asids_2 aag subjects x asid as as' ao ao'  \<equiv>
+     (riscv_asid_table as (asid_high_bits_of asid) \<noteq> riscv_asid_table as' (asid_high_bits_of asid)
+      \<longrightarrow> (\<forall>x. riscv_asid_table as' (asid_high_bits_of asid) = Some x \<longrightarrow> pasObjectAbs aag x \<in> subjects) \<and>
+          (\<forall>asid'. asid' \<noteq> 0 \<and> asid_high_bits_of asid' = asid_high_bits_of asid
+                   \<longrightarrow> pasASIDAbs aag asid' \<in> subjects)) \<and>
+     (pasObjectAbs aag x \<notin> subjects
+      \<longrightarrow> (\<forall>pool. (ao |> asid_pool_of) x = Some pool
+                  \<longrightarrow> (ao' |> asid_pool_of) x \<noteq> None \<and>
+                      (\<forall>pool'. (ao' |> asid_pool_of) x = Some pool' \<longrightarrow>
+                               (pool \<noteq> pool' \<longrightarrow> (\<exists>asid. riscv_asid_table as (asid_high_bits_of asid) = Some x)))))"
+
+lemmas integrity_asids_def = integrity_asids_2_def
+
+\<comment> \<open>No hypervisor\<close>
+definition integrity_hyp_2 ::
+  "'a PAS \<Rightarrow> 'a set \<Rightarrow> obj_ref \<Rightarrow> machine_state \<Rightarrow> machine_state \<Rightarrow> arch_state \<Rightarrow> arch_state
+          \<Rightarrow> (obj_ref \<Rightarrow> arch_kernel_obj option) \<Rightarrow> (obj_ref \<Rightarrow> arch_kernel_obj option) \<Rightarrow> bool"
+where
+  "integrity_hyp_2 aag subjects x ms ms' as as' aobjs aobjs' \<equiv> True"
+
+lemmas integrity_hyp_def = integrity_hyp_2_def
+
+definition valid_cur_hyp :: "'s::state_ext state \<Rightarrow> bool" where
+  "valid_cur_hyp \<equiv> \<top>"
+
+\<comment> \<open>No FPU\<close>
+definition integrity_fpu_2 ::
+  "'a PAS \<Rightarrow> 'a set \<Rightarrow> obj_ref \<Rightarrow> machine_state \<Rightarrow> machine_state
+          \<Rightarrow> (obj_ref \<Rightarrow> kernel_object option) \<Rightarrow> (obj_ref \<Rightarrow> kernel_object option) \<Rightarrow> bool"
+where
+  "integrity_fpu_2 aag subjects x ms ms' kh kh' \<equiv> True"
+
+lemmas integrity_fpu_def = integrity_fpu_2_def
+
+
+subsection \<open>Misc definitions\<close>
+
+abbreviation arch_IP_update where
+  "arch_IP_update a \<equiv>
+   arch_tcb_set_registers ((arch_tcb_get_registers a)(NextIP := arch_tcb_get_registers a FaultIP)) a"
+
+definition asid_pool_integrity ::
+  "'a set \<Rightarrow> 'a PAS \<Rightarrow> (asid_low_index \<rightharpoonup> obj_ref) \<Rightarrow> (asid_low_index \<rightharpoonup> obj_ref) \<Rightarrow> bool" where
+  "asid_pool_integrity subjects aag pool pool' \<equiv>
+     \<forall>x. pool' x \<noteq> pool x
+         \<longrightarrow> pool' x = None \<and> aag_subjects_have_auth_to subjects aag Control (the (pool x))"
+
+inductive arch_integrity_obj_atomic ::
+   "'a PAS \<Rightarrow> 'a set \<Rightarrow> 'a \<Rightarrow> arch_kernel_obj \<Rightarrow> arch_kernel_obj \<Rightarrow> bool"
+  for aag subjects l ao ao' where
+  arch_troa_asidpool_clear:
+    "\<lbrakk> ao = ASIDPool pool; ao' = ASIDPool pool';
+       asid_pool_integrity subjects aag pool pool' \<rbrakk>
+       \<Longrightarrow> arch_integrity_obj_atomic aag subjects l ao ao'"
+
+inductive arch_integrity_obj_alt ::
+   "'a PAS \<Rightarrow> 'a set \<Rightarrow> 'a \<Rightarrow> arch_kernel_obj \<Rightarrow> arch_kernel_obj \<Rightarrow> bool"
+  for aag subjects l' ao ao' where
+  arch_tro_alt_asidpool_clear:
+    "\<lbrakk> ao = ASIDPool pool; ao' = ASIDPool pool';
+       asid_pool_integrity subjects aag pool pool'\<rbrakk>
+       \<Longrightarrow> arch_integrity_obj_alt aag subjects l' ao ao'"
+
+definition auth_ipc_buffers :: "'z::state_ext state \<Rightarrow> obj_ref \<Rightarrow> obj_ref set" where
+  "auth_ipc_buffers s \<equiv> \<lambda>p. case (get_tcb p s) of
+     None \<Rightarrow> {}
+   | Some tcb \<Rightarrow>
+     (case tcb_ipcframe tcb of
+        ArchObjectCap (FrameCap p' R vms False _) \<Rightarrow>
+          if AllowWrite \<in> R
+          then (ptr_range (p' + (tcb_ipc_buffer tcb && mask (pageBitsForSize vms))) msg_align_bits)
+          else {}
+      | _ \<Rightarrow> {})"
+
+end
+
+end
---
+++ proof/access-control/AARCH64/ArchSyscall_AC.thy
@@ -0,0 +1,205 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchSyscall_AC
+imports Syscall_AC
+begin
+
+context Arch begin arch_global_naming
+
+named_theorems Syscall_AC_assms
+
+crunch set_original
+  for idle_thread[wp]: "\<lambda>s. P (idle_thread s)"
+  and cur_thread[wp]: "\<lambda>s. P (cur_thread s)"
+
+crunch prepare_thread_delete
+  for idle_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (idle_thread s)"
+
+lemma cap_move_idle_thread[Syscall_AC_assms, wp]:
+  "cap_move new_cap src_slot dest_slot \<lbrace>\<lambda>s. P (idle_thread s)\<rbrace>"
+  unfolding cap_move_def
+  by (wpsimp wp: dxo_wp_weak)
+
+lemma cancel_badged_sends_idle_thread[Syscall_AC_assms, wp]:
+  "cancel_badged_sends epptr badge \<lbrace>\<lambda>s. P (idle_thread s)\<rbrace>"
+  unfolding cancel_badged_sends_def
+  by (wpsimp wp: dxo_wp_weak mapM_wp_inv get_simple_ko_wp simp: filterM_mapM)
+
+declare arch_finalise_cap_idle_thread[Syscall_AC_assms]
+
+lemma invs_irq_state_update[Syscall_AC_assms, simp]:
+  "invs (s\<lparr>machine_state := irq_state_update f sa\<rparr>) = invs (s\<lparr>machine_state := sa\<rparr>)"
+  apply (rule iffI)
+   apply (subst invs_irq_state_independent[symmetric])
+   apply (subst AARCH64.fold_congs(2); fastforce)
+  apply (subst (asm) invs_irq_state_independent[symmetric])
+  apply (subst AARCH64.fold_congs(2); fastforce)
+  done
+
+crunch prepare_thread_delete, arch_finalise_cap
+  for cur_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (cur_thread s)"
+  (wp: crunch_wps simp: crunch_simps)
+
+lemma cap_move_cur_thread[Syscall_AC_assms, wp]:
+  "cap_move new_cap src_slot dest_slot \<lbrace>\<lambda>s. P (cur_thread s)\<rbrace>"
+  unfolding cap_move_def
+  by (wpsimp wp: dxo_wp_weak)
+
+lemma cancel_badged_sends_cur_thread[Syscall_AC_assms, wp]:
+  "cancel_badged_sends epptr badge \<lbrace>\<lambda>s. P (cur_thread s)\<rbrace>"
+  unfolding cancel_badged_sends_def
+  by (wpsimp wp: dxo_wp_weak filterM_preserved crunch_wps)
+
+crunch arch_mask_irq_signal, handle_reserved_irq
+  for pas_refined[Syscall_AC_assms, wp]: "pas_refined aag"
+
+crunch handle_hypervisor_fault
+  for pas_refined[Syscall_AC_assms, wp]: "pas_refined aag"
+  and cur_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (cur_thread s)"
+  and integrity[Syscall_AC_assms, wp]: "integrity aag X st"
+
+crunch handle_vm_fault
+  for pas_refined[Syscall_AC_assms, wp]: "pas_refined aag"
+  and cur_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (cur_thread s)"
+  and state_refs_of[Syscall_AC_assms, wp]: "\<lambda>s. P (state_refs_of s)"
+
+lemma handle_vm_fault_integrity[Syscall_AC_assms]:
+  "\<lbrace>integrity aag X st and K (is_subject aag thread)\<rbrace>
+   handle_vm_fault thread vmfault_type
+   \<lbrace>\<lambda>rv. integrity aag X st\<rbrace>"
+  unfolding handle_vm_fault_def
+  by (cases vmfault_type; wpsimp wp: as_user_integrity_autarch dmo_wp)
+
+crunch ackInterrupt, resetTimer
+  for underlying_memory_inv[Syscall_AC_assms, wp]: "\<lambda>s. P (underlying_memory s)"
+  (simp: maskInterrupt_def)
+
+crunch arch_mask_irq_signal, handle_reserved_irq
+  for integrity[Syscall_AC_assms, wp]: "integrity aag X st"
+  (wp: dmo_no_mem_respects)
+
+lemma set_thread_state_restart_to_running_respects[Syscall_AC_assms]:
+  "\<lbrace>integrity aag X st and st_tcb_at ((=) Restart) thread and K (pasMayActivate aag)\<rbrace>
+   do pc \<leftarrow> as_user thread getRestartPC;
+            as_user thread $ setNextPC pc;
+            set_thread_state thread Structures_A.thread_state.Running
+   od
+   \<lbrace>\<lambda>_. integrity aag X st\<rbrace>"
+  apply (simp add: set_thread_state_def as_user_def split_def setNextPC_def
+                   getRestartPC_def setRegister_def bind_assoc getRegister_def)
+  apply (wpsimp wp: set_object_wp)
+  apply (clarsimp simp: in_monad fun_upd_def[symmetric] cong: if_cong)
+  apply (cases "is_subject aag thread")
+   apply (cut_tac aag=aag in integrity_update_autarch, simp+)
+  apply (erule integrity_trans)
+  apply (clarsimp simp: integrity_def integrity_asids_def obj_at_def st_tcb_at_def opt_map_def)
+  apply (clarsimp dest!: get_tcb_SomeD)
+  apply (rule_tac tro_tcb_activate[OF refl refl])
+    apply (simp add: tcb_bound_notification_reset_integrity_def arch_tcb_get_registers_def
+                     arch_tcb_set_registers_def arch_tcb_context_get_def arch_tcb_context_set_def)+
+  done
+
+lemma getActiveIRQ_inv[Syscall_AC_assms]:
+  "\<forall>f s. P s \<longrightarrow> P (irq_state_update f s)
+   \<Longrightarrow> \<lbrace>P\<rbrace> getActiveIRQ irq \<lbrace>\<lambda>rv. P\<rbrace>"
+  by (wpsimp simp: irq_state_independent_def)
+
+lemma getActiveIRQ_rv_None[Syscall_AC_assms]:
+  "\<lbrace>\<top>\<rbrace> getActiveIRQ True \<lbrace>\<lambda>rv ms. (rv \<noteq> None \<longrightarrow> the rv \<notin> non_kernel_IRQs)\<rbrace>"
+  by (wpsimp simp: getActiveIRQ_def)
+
+lemma arch_activate_idle_thread_respects[Syscall_AC_assms, wp]:
+  "arch_activate_idle_thread t \<lbrace>integrity aag X st\<rbrace>"
+  unfolding arch_activate_idle_thread_def by wpsimp
+
+lemma arch_activate_idle_thread_pas_refined[Syscall_AC_assms, wp]:
+  "arch_activate_idle_thread t \<lbrace>pas_refined aag\<rbrace>"
+  unfolding arch_activate_idle_thread_def by wpsimp
+
+lemma arch_switch_to_thread_respects[Syscall_AC_assms, wp]:
+  "arch_switch_to_thread t \<lbrace>integrity aag X st\<rbrace>"
+  unfolding arch_switch_to_thread_def by wpsimp
+
+lemma arch_switch_to_thread_pas_refined[Syscall_AC_assms, wp]:
+  "arch_switch_to_thread t \<lbrace>pas_refined aag\<rbrace>"
+  unfolding arch_switch_to_thread_def by wpsimp
+
+lemma arch_switch_to_idle_thread_respects[Syscall_AC_assms, wp]:
+  "arch_switch_to_idle_thread \<lbrace>integrity aag X st\<rbrace>"
+  unfolding arch_switch_to_idle_thread_def by wpsimp
+
+lemma arch_switch_to_idle_thread_pas_refined[Syscall_AC_assms, wp]:
+  "arch_switch_to_idle_thread \<lbrace>pas_refined aag\<rbrace>"
+  unfolding arch_switch_to_idle_thread_def by wpsimp
+
+lemma arch_mask_irq_signal_arch_state[Syscall_AC_assms, wp]:
+  "arch_mask_irq_signal irq \<lbrace>\<lambda>s :: det_ext state. P (arch_state s)\<rbrace>"
+  by wpsimp
+
+lemma handle_reserved_irq_arch_state[Syscall_AC_assms, wp]:
+  "handle_reserved_irq irq \<lbrace>\<lambda>s :: det_ext state. P (arch_state s)\<rbrace>"
+  unfolding handle_reserved_irq_def by wpsimp
+
+crunch arch_post_cap_deletion
+  for ct_active[Syscall_AC_assms, wp]: "ct_active"
+  (wp: crunch_wps filterM_preserved unless_wp simp: crunch_simps ignore: do_extended_op)
+
+crunch
+  arch_post_modify_registers, arch_invoke_irq_control,
+  arch_invoke_irq_handler, arch_perform_invocation, arch_mask_irq_signal,
+  handle_reserved_irq, handle_vm_fault, handle_hypervisor_fault, handle_arch_fault_reply,
+  arch_prepare_set_domain, arch_post_set_flags, arch_prepare_next_domain
+  for cur_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (cur_thread s)"
+  and idle_thread[Syscall_AC_assms, wp]: "\<lambda>s. P (idle_thread s)"
+  and cur_domain[Syscall_AC_assms, wp]:  "\<lambda>s. P (cur_domain s)"
+  (wp: crunch_wps simp: crunch_simps)
+
+\<comment> \<open>These aren't proved in the previous crunch, and hence need to be declared\<close>
+declare handle_arch_fault_reply_cur_thread[Syscall_AC_assms]
+declare handle_arch_fault_reply_it[Syscall_AC_assms]
+declare arch_prepare_set_domain_idle_thread[Syscall_AC_assms]
+declare init_arch_objects_inv[Syscall_AC_assms]
+declare arch_post_set_flags_inv[Syscall_AC_assms]
+
+lemma ackInterrupt_integrity[Syscall_AC_assms,wp]:
+  "do_machine_op (ackInterrupt irq) \<lbrace>integrity aag X st\<rbrace>"
+  by (wpsimp wp: dmo_no_mem_respects)
+
+lemma maskInterrupt_integrity[Syscall_AC_assms,wp]:
+  "do_machine_op (maskInterrupt m irq) \<lbrace>integrity aag X st\<rbrace>"
+  by (wpsimp wp: dmo_no_mem_respects)
+
+lemma resetTimer_integrity[Syscall_AC_assms,wp]:
+  "do_machine_op resetTimer \<lbrace>integrity aag X st\<rbrace>"
+  by (wpsimp wp: dmo_no_mem_respects)
+
+lemma valid_cur_hyp_triv[Syscall_AC_assms]:
+  "f \<lbrace>\<lambda>s. P (valid_cur_hyp s)\<rbrace>"
+  by (wpsimp simp: valid_cur_hyp_def)
+
+lemma valid_cur_hyp_machine_state[Syscall_AC_assms]:
+  "valid_cur_hyp (machine_state_update f s) = valid_cur_hyp s"
+  by (simp add: valid_cur_hyp_def)
+
+crunch arch_prepare_next_domain
+  for pas_refined[Syscall_AC_assms, wp]: "pas_refined aag"
+  and integrity[Syscall_AC_assms, wp]: "integrity aag X st"
+  and ct_not_in_q[Syscall_AC_assms, wp]: ct_not_in_q
+  and valid_sched_action[Syscall_AC_assms, wp]: valid_sched_action
+  and ct_in_cur_domain[Syscall_AC_assms, wp]: ct_in_cur_domain
+
+end
+
+
+global_interpretation Syscall_AC_1?: Syscall_AC_1
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Syscall_AC_assms | solves \<open>wp only: Syscall_AC_assms; simp\<close>)?)
+qed
+
+end
