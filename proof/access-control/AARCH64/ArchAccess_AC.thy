e1afd4edd7738c1d8a70b631f8d3327083573375
Ryan Barry <ryan.barry@proofcraft.systems>
2025-04-22 15:50:01 +1000

    aarch64 access: copy riscv64 theories
    This commit mostly serves as a reference point for reviewing AARCH64
    access control changes. The theories here were taken from a later commit
    where the RISCV64 access control proofs had been updated and refactored.
    
    Signed-off-by: Ryan Barry <ryan.barry@proofcraft.systems>

---
+++ proof/access-control/AARCH64/ArchAccess_AC.thy
@@ -0,0 +1,304 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchAccess_AC
+imports Access_AC
+begin
+
+section\<open>Arch-specific AC proofs\<close>
+
+context Arch begin arch_global_naming
+
+named_theorems Access_AC_assms
+
+lemma acap_class_reply[Access_AC_assms]:
+  "acap_class acap \<noteq> ReplyClass t"
+  by (cases acap; simp)
+
+lemma arch_troa_tro_alt[Access_AC_assms, elim!]:
+  "arch_integrity_obj_atomic aag subjects l ko ko'
+   \<Longrightarrow> arch_integrity_obj_alt aag subjects l ko ko'"
+  by (fastforce elim: arch_integrity_obj_atomic.cases intro: arch_integrity_obj_alt.intros)
+
+lemma clear_asidpool_trans[elim]:
+  "\<lbrakk> asid_pool_integrity subjects aag pool pool';
+     asid_pool_integrity subjects aag pool' pool'' \<rbrakk>
+     \<Longrightarrow> asid_pool_integrity subjects aag pool pool''"
+  apply (clarsimp simp: asid_pool_integrity_def)
+  apply (erule_tac x=x in allE)+
+  apply (fastforce split: option.split_asm)
+  done
+
+lemma cap_asid'_member[simp]:
+  "asid \<in> cap_asid' cap = (\<exists>acap. cap = ArchObjectCap acap \<and> asid \<in> acap_asid' acap)"
+  by (cases cap; clarsimp)
+
+lemma clas_caps_of_state[Access_AC_assms]:
+  "\<lbrakk> caps_of_state s slot = Some cap; pas_refined aag s \<rbrakk>
+     \<Longrightarrow> cap_links_asid_slot aag (pasObjectAbs aag (fst slot)) cap"
+  apply (clarsimp simp: cap_links_asid_slot_def label_owns_asid_slot_def pas_refined_def)
+  apply (drule state_asids_to_policy_aux.intros)
+   apply assumption
+  apply (blast dest: state_asids_to_policy_aux.intros)
+  done
+
+lemma arch_tro_alt_trans_spec[Access_AC_assms]:
+  "\<lbrakk> arch_integrity_obj_alt aag subjects l ko ko';
+     arch_integrity_obj_alt aag subjects l ko' ko'' \<rbrakk>
+     \<Longrightarrow> arch_integrity_obj_alt aag subjects l ko ko''"
+  by (fastforce simp: arch_integrity_obj_alt.simps)
+
+lemma tcb_hyp_refs_arch_tcb_set_registers[Access_AC_assms]:
+  "tcb_hyp_refs (arch_tcb_set_registers regs atcb) = tcb_hyp_refs atcb"
+  by (simp add: arch_tcb_set_registers_def)
+
+end
+
+
+global_interpretation Access_AC_1?: Access_AC_1
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Access_AC_assms)?)
+qed
+
+
+context Arch begin arch_global_naming
+
+lemma auth_ipc_buffers_tro[Access_AC_assms]:
+  "\<lbrakk> integrity_obj_state aag activate subjects s s';
+     x \<in> auth_ipc_buffers s' p; pasObjectAbs aag p \<notin> subjects \<rbrakk>
+     \<Longrightarrow> x \<in> auth_ipc_buffers s p "
+  by (drule_tac x = p in spec)
+     (erule integrity_objE;
+      fastforce simp: tcb_states_of_state_def get_tcb_def auth_ipc_buffers_def
+               split: cap.split_asm arch_cap.split_asm if_split_asm bool.splits)
+
+lemma trasids_trans[Access_AC_assms]:
+  "\<lbrakk> \<forall>x a. integrity_asids_2 aag subjects x a as as' ao ao';
+     \<forall>x a. integrity_asids_2 aag subjects x a as' as'' ao' ao'' \<rbrakk>
+     \<Longrightarrow> \<forall>x a. integrity_asids_2 aag subjects x a as as'' ao ao''"
+  by (clarsimp simp: integrity_asids_def) metis
+
+lemma integrity_asids_refl[Access_AC_assms, simp]:
+  "integrity_asids aag subjects x a s s"
+  by (simp add: integrity_asids_def)
+
+lemma integrity_asids_update_autarch[Access_AC_assms]:
+  "\<lbrakk> integrity_asids_2 aag subjects x a as as' ao ao'; pasObjectAbs aag ptr \<in> subjects \<rbrakk>
+     \<Longrightarrow> integrity_asids_2 aag subjects x a as as' ao (ao'(ptr := ako))"
+  by (auto simp: integrity_asids_def opt_map_def)
+
+lemma integrity_hyp_triv[intro!,simp]:
+  "integrity_hyp_2 aag subjects x ms ms' as as' ao ao'"
+  by (simp add: integrity_hyp_def)
+
+lemma integrity_fpu_triv[intro!,simp]:
+  "integrity_fpu_2 aag subjects x as as' kh kh'"
+  by (simp add: integrity_fpu_def)
+
+lemma integrity_hyp_wp_triv:
+  "\<lbrace>P\<rbrace> c \<lbrace>\<lambda>_ s. integrity_hyp aag subjects x st (f s)\<rbrace>"
+  by wpsimp
+
+lemma integrity_fpu_wp_triv:
+  "\<lbrace>P\<rbrace> c \<lbrace>\<lambda>_ s. integrity_fpu aag subjects x st (f s)\<rbrace>"
+  by wpsimp
+
+lemma assumption2:
+  "P \<Longrightarrow> Q \<Longrightarrow> P"
+  by simp
+
+lemma integrity_hyp_eq_triv:
+  "integrity_hyp_2 a b c d e f g h i = integrity_hyp_2 r s t u v w x y z"
+  by simp
+
+lemma integrity_fpu_eq_triv:
+  "integrity_fpu_2 a b c d e f g = integrity_fpu_2 t u v w x y z"
+  by simp
+
+lemmas integrity_arch_triv =
+  integrity_hyp_wp_triv
+  integrity_fpu_wp_triv
+  integrity_hyp_triv
+  assumption2[OF integrity_hyp_triv]
+  assumption2[OF assumption2[OF integrity_hyp_triv]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_hyp_triv]]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_hyp_triv]]]
+  integrity_fpu_triv
+  assumption2[OF integrity_fpu_triv]
+  assumption2[OF assumption2[OF integrity_fpu_triv]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_fpu_triv]]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_fpu_triv]]]
+  integrity_hyp_eq_triv
+  assumption2[OF integrity_hyp_eq_triv]
+  assumption2[OF assumption2[OF integrity_hyp_eq_triv]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_hyp_eq_triv]]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_hyp_eq_triv]]]
+  integrity_fpu_eq_triv
+  assumption2[OF integrity_fpu_eq_triv]
+  assumption2[OF assumption2[OF integrity_fpu_eq_triv]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_fpu_eq_triv]]]
+  assumption2[OF assumption2[OF assumption2[OF integrity_fpu_eq_triv]]]
+
+declare integrity_arch_triv[Access_AC_assms]
+
+end
+
+
+global_interpretation Access_AC_2?: Access_AC_2
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Access_AC_assms)?)
+qed
+
+
+context Arch begin arch_global_naming
+
+lemma ipcframe_subset_page:
+  "\<lbrakk> valid_objs s; get_tcb p s = Some tcb;
+     tcb_ipcframe tcb = ArchObjectCap (FrameCap p' R vms d xx);
+     x \<in> ptr_range (p' + (tcb_ipc_buffer tcb && mask (pageBitsForSize vms))) msg_align_bits \<rbrakk>
+     \<Longrightarrow> x \<in> ptr_range p' (pageBitsForSize vms)"
+   apply (frule (1) valid_tcb_objs)
+   apply (clarsimp simp add: valid_tcb_def ran_tcb_cap_cases)
+   apply (erule set_mp[rotated])
+   apply (rule ptr_range_subset)
+     apply (simp add: valid_cap_def cap_aligned_def)
+    apply (simp add: valid_tcb_def valid_ipc_buffer_cap_def is_aligned_andI1 split:bool.splits)
+   apply (rule order_trans [OF _ pbfs_atleast_pageBits])
+   apply (simp add: msg_align_bits pageBits_def)
+  apply (rule and_mask_less')
+  apply (simp add: pbfs_less_wb' [unfolded word_bits_conv])
+  done
+
+lemma auth_ipc_buffers_member_def:
+  "x \<in> auth_ipc_buffers s p =
+   (\<exists>tcb p' R vms xx. get_tcb p s = Some tcb
+                   \<and> tcb_ipcframe tcb = (ArchObjectCap (FrameCap p' R vms False xx))
+                   \<and> caps_of_state s (p, tcb_cnode_index 4) =
+                      Some (ArchObjectCap (FrameCap p' R vms False xx))
+                   \<and> AllowWrite \<in> R
+                   \<and> x \<in> ptr_range (p' + (tcb_ipc_buffer tcb && mask (pageBitsForSize vms)))
+                                    msg_align_bits)"
+  unfolding auth_ipc_buffers_def
+  by (clarsimp simp: caps_of_state_tcb' split: option.splits cap.splits arch_cap.splits bool.splits)
+
+lemma auth_ipc_buffers_member[Access_AC_assms]:
+  "\<lbrakk> x \<in> auth_ipc_buffers s p; valid_objs s \<rbrakk>
+     \<Longrightarrow> \<exists>tcb acap. get_tcb p s = Some tcb
+                  \<and> tcb_ipcframe tcb = (ArchObjectCap acap)
+                  \<and> caps_of_state s (p, tcb_cnode_index 4) = Some (ArchObjectCap acap)
+                  \<and> Write \<in> arch_cap_auth_conferred acap
+                  \<and> x \<in> aobj_ref' acap"
+  by (fastforce simp: auth_ipc_buffers_def caps_of_state_tcb' arch_cap_auth_conferred_def
+                      vspace_cap_rights_to_auth_def ipcframe_subset_page
+               split: option.splits cap.splits arch_cap.splits bool.splits if_splits)
+
+lemma asid_pool_integrity_mono[Access_AC_assms]:
+  "\<lbrakk> asid_pool_integrity S aag cont cont'; S \<subseteq> T \<rbrakk> \<Longrightarrow> asid_pool_integrity T aag cont cont'"
+  unfolding asid_pool_integrity_def by fastforce
+
+lemma integrity_asids_mono[Access_AC_assms]:
+    "\<lbrakk> integrity_asids aag S x a s s'; S \<subseteq> T; pas_refined aag s; valid_objs s \<rbrakk>
+       \<Longrightarrow> integrity_asids aag T x a s s'"
+  by (fastforce simp: integrity_asids_def)
+
+lemma arch_integrity_obj_atomic_mono[Access_AC_assms]:
+  "\<lbrakk> arch_integrity_obj_atomic aag S l ao ao'; S \<subseteq> T; pas_refined aag s; valid_objs s \<rbrakk>
+     \<Longrightarrow> arch_integrity_obj_atomic aag T l ao ao'"
+  by (clarsimp simp: arch_integrity_obj_atomic.simps asid_pool_integrity_mono)
+
+end
+
+
+global_interpretation Access_AC_3?: Access_AC_3
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Access_AC_assms)?)
+qed
+
+context Arch_p_arch_update_eq begin arch_global_naming
+
+lemma state_vrefs[Access_AC_assms, iff]:
+  "state_vrefs (f s) = state_vrefs s"
+  by (simp add: state_vrefs_def pspace)
+
+end
+
+context Arch begin arch_global_naming
+
+lemma integrity_asids_kh_upd_None[Access_AC_assms]:
+  "\<lbrakk> ao' p = None; integrity_asids_2 aag subjects x a as as' ao ao'\<rbrakk>
+     \<Longrightarrow> integrity_asids_2 aag subjects x a as as' (ao(p := None)) ao'"
+  "\<lbrakk> ao p = None; integrity_asids_2 aag subjects x a as as' ao ao'\<rbrakk>
+     \<Longrightarrow> integrity_asids_2 aag subjects x a as as' ao (ao'(p := None)) "
+  unfolding integrity_asids_def opt_map_def
+  by (auto split: option.split_asm)
+
+end
+
+
+global_interpretation Access_AC_4?: Access_AC_4
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Access_AC_assms)?)
+qed
+
+
+locale Arch_pas_refined_arch_update_eq = Arch +
+  fixes f :: "('a \<Rightarrow> 'a) \<Rightarrow> arch_state \<Rightarrow> arch_state"
+  assumes atable: "riscv_asid_table (f g ms) = riscv_asid_table ms"
+begin
+
+lemma vs_lookup_table_arch_upd:
+  "vs_lookup_table lvl asid vref (arch_state_update (\<lambda>ms. f (g ms) (h ms)) s) =
+   vs_lookup_table lvl asid vref (arch_state_update h s)"
+  by (auto simp: vs_lookup_table_eq_lift pool_for_asid_def atable)
+
+lemma pas_refined_arch_upd:
+  "pas_refined aag (arch_state_update (\<lambda>ms. f (g ms) (h ms)) s) =
+   pas_refined aag (arch_state_update h s)"
+  unfolding pas_refined_def state_objs_to_policy_def state_vrefs_def
+  by (auto simp: vs_lookup_table_arch_upd atable)
+
+lemma arch_state_update_id:
+  "arch_state_update id s = s"
+  by simp
+
+lemmas [simp] =
+  pas_refined_arch_upd[where h="\<lambda>_. _"]
+  pas_refined_arch_upd[where h="\<lambda>_. _" and g="\<lambda>_. _"]
+  pas_refined_arch_upd[where h=id, simplified arch_state_update_id id_apply]
+  pas_refined_arch_upd[where h=id and g="\<lambda>_. _", simplified arch_state_update_id id_apply]
+
+end
+
+sublocale Arch \<subseteq> global_pts_update: Arch_pas_refined_arch_update_eq riscv_global_pts_update by unfold_locales auto
+sublocale Arch \<subseteq> kernel_vspace_update: Arch_pas_refined_arch_update_eq riscv_kernel_vspace_update by unfold_locales auto
+
+
+context Arch begin arch_global_naming
+
+lemma dmo_no_mem_respects:
+  assumes p: "\<And>P. mop \<lbrace>\<lambda>ms. P (underlying_memory ms)\<rbrace>"
+  assumes q: "\<And>P. mop \<lbrace>\<lambda>ms. P (device_state ms)\<rbrace>"
+  shows "do_machine_op mop \<lbrace>integrity aag X st\<rbrace>"
+  unfolding integrity_def tcb_states_of_state_def get_tcb_def
+  by (wpsimp wp: dmo_machine_state_lift | wps assms)+
+
+lemma state_vrefsD:
+  "\<lbrakk> vs_lookup_table level asid vref s = Some (lvl, p);
+     aobjs_of s p = Some ao; vref \<in> user_region; x \<in> vs_refs_aux lvl ao \<rbrakk>
+     \<Longrightarrow> x \<in> state_vrefs s p"
+  unfolding state_vrefs_def by fastforce
+
+end
+
+end
---
+++ proof/access-control/AARCH64/ArchIpc_AC.thy
@@ -0,0 +1,250 @@
+(*
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchIpc_AC
+imports Ipc_AC
+begin
+
+context Arch begin arch_global_naming
+
+named_theorems Ipc_AC_assms
+
+declare make_fault_msg_inv[Ipc_AC_assms]
+declare handle_arch_fault_reply_typ_at[Ipc_AC_assms]
+
+crunch cap_insert_ext
+  for integrity_asids[Ipc_AC_assms, wp]: "integrity_asids aag subjects x a st"
+  (ignore_del: cap_insert_ext) \<comment> \<open>FIXME: should these ext consts still be ignored in DetSched_AI files and beyond?\<close>
+
+lemma arch_derive_cap_auth_derived[Ipc_AC_assms]:
+  "\<lbrace>\<top>\<rbrace> arch_derive_cap acap \<lbrace>\<lambda>rv _. rv \<noteq> NullCap \<longrightarrow> auth_derived rv (ArchObjectCap acap)\<rbrace>, -"
+  by (case_tac acap;
+      simp add: derive_cap_def arch_derive_cap_def;
+      wpc?;
+      wp?;
+      simp add: auth_derived_def cap_auth_conferred_def arch_cap_auth_conferred_def)
+
+lemma lookup_ipc_buffer_has_auth[Ipc_AC_assms, wp]:
+  "\<lbrace>pas_refined aag and valid_objs\<rbrace>
+   lookup_ipc_buffer True receiver
+   \<lbrace>\<lambda>rv _. ipc_buffer_has_auth aag receiver rv\<rbrace>"
+  apply (rule hoare_pre)
+   apply (simp add: lookup_ipc_buffer_def)
+   apply (wp get_cap_wp thread_get_wp' | wpc)+
+  apply (clarsimp simp: cte_wp_at_caps_of_state ipc_buffer_has_auth_def get_tcb_ko_at[symmetric])
+  apply (frule caps_of_state_tcb_cap_cases [where idx="tcb_cnode_index 4"])
+   apply (simp add: dom_tcb_cap_cases)
+  apply (frule (1) caps_of_state_valid_cap)
+  apply (rule conjI)
+   apply (clarsimp simp: valid_cap_simps cap_aligned_def)
+   apply (erule aligned_add_aligned)
+    apply (rule is_aligned_andI1)
+    apply (drule (1) valid_tcb_objs)
+    apply (clarsimp simp: valid_obj_def valid_tcb_def valid_ipc_buffer_cap_def
+                   split: if_splits)
+   apply (rule order_trans [OF _ pbfs_atleast_pageBits])
+   apply (simp add: msg_align_bits pageBits_def)
+  apply simp
+  apply (drule (1) cap_auth_caps_of_state)
+  apply (clarsimp simp: aag_cap_auth_def cap_auth_conferred_def arch_cap_auth_conferred_def
+                        vspace_cap_rights_to_auth_def vm_read_write_def)
+  apply (drule bspec)
+   apply (erule (3) ipcframe_subset_page)
+  apply simp
+  done
+
+lemma arch_tcb_get_set_registers[Ipc_AC_assms,simp]:
+  "arch_tcb_get_registers (arch_tcb_set_registers regs atcb) = regs"
+  by (clarsimp simp: arch_tcb_get_registers_def arch_tcb_set_registers_def)
+
+lemma arch_tcb_set_get_registers[Ipc_AC_assms,simp]:
+  "arch_tcb_set_registers (arch_tcb_get_registers atcb) atcb = atcb"
+  by (simp add: arch_tcb_set_registers_def arch_tcb_get_registers_def)
+
+lemma arch_tcb_set_set_registers[Ipc_AC_assms,simp]:
+  "arch_tcb_set_registers regs (arch_tcb_set_registers regs' atcb) =
+   arch_tcb_set_registers regs atcb"
+  by (auto simp: arch_tcb_set_registers_def)
+
+lemma arch_tcb_context_set_set_registers[Ipc_AC_assms,simp]:
+  "arch_tcb_context_set (arch_tcb_context_get (arch_tcb_set_registers regs atcb)) atcb =
+   arch_tcb_set_registers regs atcb"
+  by (auto simp: arch_tcb_context_set_def arch_tcb_context_get_def arch_tcb_set_registers_def)
+
+lemma arch_tcb_setRegister[Ipc_AC_assms]:
+  "((), uc) \<in> fst (setRegister r v (arch_tcb_context_get atcb))
+   \<Longrightarrow> uc = arch_tcb_context_get (arch_tcb_set_registers ((arch_tcb_get_registers atcb)(r := v)) atcb)"
+  by (simp add: arch_tcb_context_get_def arch_tcb_context_set_def
+                arch_tcb_get_registers_def arch_tcb_set_registers_def
+                setRegister_def modify_def get_def put_def bind_def)
+end
+
+
+global_interpretation Ipc_AC_1?: Ipc_AC_1
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Ipc_AC_assms)?)
+qed
+
+
+context Arch begin arch_global_naming
+
+lemma store_word_offs_respects_in_ipc[Ipc_AC_assms]:
+  "\<lbrace>integrity_tcb_in_ipc aag X receiver epptr TRContext st and
+    K ((\<not> is_subject aag receiver \<longrightarrow> auth_ipc_buffers st receiver = ptr_range buf msg_align_bits)
+        \<and> is_aligned buf msg_align_bits \<and> r < 2 ^ (msg_align_bits - word_size_bits))\<rbrace>
+   store_word_offs buf r v
+   \<lbrace>\<lambda>_. integrity_tcb_in_ipc aag X receiver epptr TRContext st\<rbrace>"
+  apply (simp add: store_word_offs_def storeWord_def pred_conj_def)
+  apply (wp dmo_wp)
+  apply (clarsimp simp: integrity_tcb_in_ipc_def)
+  apply (erule integrity_trans)
+  apply (clarsimp simp: integrity_def integrity_asids_def)
+  apply (subgoal_tac "\<forall>i \<in> set [0..7].
+                      buf + of_nat r * of_nat word_size + of_int i \<in> ptr_range buf msg_align_bits")
+   apply (fastforce simp: word_rsplit_0 upto.simps atLeastAtMost_upto)
+  apply (fastforce simp add: unat_def word_size_def of_nat_nat[symmetric] word_of_nat_less
+                   simp del: of_nat_nat intro: ptr_range_off_off_mems)
+  done
+
+crunch set_extra_badge
+  for respects_in_ipc[Ipc_AC_assms, wp]: "integrity_tcb_in_ipc aag X receiver epptr TRContext st"
+  (wp: store_word_offs_respects_in_ipc)
+
+crunch handle_arch_fault_reply
+  for pas_refined[Ipc_AC_assms, wp]: "pas_refined aag"
+
+lemma set_mrs_respects_in_ipc[Ipc_AC_assms]:
+  "\<lbrace>integrity_tcb_in_ipc aag X receiver epptr TRContext st and
+    K ((\<not> is_subject aag receiver \<longrightarrow>
+        (case recv_buf of None \<Rightarrow> True | Some buf' \<Rightarrow> auth_ipc_buffers st receiver =
+                                                     ptr_range buf' msg_align_bits)) \<and>
+       (case recv_buf of None \<Rightarrow> True | Some buf' \<Rightarrow> is_aligned buf' msg_align_bits))\<rbrace>
+     set_mrs receiver recv_buf msgs
+   \<lbrace>\<lambda>rv. integrity_tcb_in_ipc aag X receiver epptr TRContext st\<rbrace>"
+  unfolding set_mrs_def set_object_def get_object_def
+  apply (rule hoare_gen_asm)
+  apply (wp mapM_x_wp' store_word_offs_respects_in_ipc
+         | wpc
+         | simp split del: if_split add: zipWithM_x_mapM_x split_def)+
+   apply (clarsimp simp add: set_zip nth_append simp: msg_align_bits' msg_max_length_def
+                   split: if_split_asm)
+   apply (simp add: msg_registers_def msgRegisters_def upto_enum_def fromEnum_def enum_register)
+   apply arith
+   apply simp
+   apply wp+
+  apply (clarsimp simp: arch_tcb_set_registers_def)
+  apply (rule update_tcb_context_in_ipc [unfolded fun_upd_def]; fastforce simp: arch_tcb_set_registers_def)
+  done
+
+lemma lookup_ipc_buffer_ptr_range_in_ipc[Ipc_AC_assms]:
+  "\<lbrace>valid_objs and integrity_tcb_in_ipc aag X thread epptr tst st\<rbrace>
+   lookup_ipc_buffer True thread
+   \<lbrace>\<lambda>rv _. \<not> is_subject aag thread \<longrightarrow>
+           (case rv of None \<Rightarrow> True | Some buf' \<Rightarrow> auth_ipc_buffers st thread =
+                                                   ptr_range buf' msg_align_bits)\<rbrace>"
+  unfolding lookup_ipc_buffer_def
+  apply (rule hoare_pre)
+   apply (wp get_cap_wp thread_get_wp' | wpc)+
+  apply (clarsimp simp: cte_wp_at_caps_of_state ipc_buffer_has_auth_def get_tcb_ko_at [symmetric])
+  apply (frule caps_of_state_tcb_cap_cases [where idx = "tcb_cnode_index 4"])
+   apply (simp add: dom_tcb_cap_cases)
+  apply (clarsimp simp: auth_ipc_buffers_def get_tcb_ko_at [symmetric] integrity_tcb_in_ipc_def)
+  apply (drule get_tcb_SomeD)
+  apply (erule(1) valid_objsE)
+  apply (clarsimp simp: valid_obj_def valid_tcb_def valid_ipc_buffer_cap_def case_bool_if
+                 split: if_split_asm)
+  apply (erule tcb_in_ipc.cases; clarsimp simp: get_tcb_def vm_read_write_def)
+  done
+
+lemma lookup_ipc_buffer_aligned[Ipc_AC_assms]:
+  "\<lbrace>valid_objs\<rbrace>
+   lookup_ipc_buffer True thread
+   \<lbrace>\<lambda>rv _. (case rv of None \<Rightarrow> True | Some buf' \<Rightarrow> is_aligned buf' msg_align_bits)\<rbrace>"
+  unfolding lookup_ipc_buffer_def
+  apply (rule hoare_pre)
+   apply (wp get_cap_wp thread_get_wp' | wpc)+
+  apply (clarsimp simp: cte_wp_at_caps_of_state get_tcb_ko_at [symmetric])
+  apply (frule caps_of_state_tcb_cap_cases [where idx = "tcb_cnode_index 4"])
+   apply (simp add: dom_tcb_cap_cases)
+  apply (frule (1) caps_of_state_valid_cap)
+  apply (clarsimp simp: valid_cap_simps cap_aligned_def)
+  apply (erule aligned_add_aligned)
+   apply (rule is_aligned_andI1)
+   apply (drule (1) valid_tcb_objs)
+   apply (clarsimp simp: valid_obj_def valid_tcb_def valid_ipc_buffer_cap_def
+                  split: if_splits)
+  apply (rule order_trans [OF _ pbfs_atleast_pageBits])
+  apply (simp add: msg_align_bits pageBits_def)
+  done
+
+lemma handle_arch_fault_reply_respects[Ipc_AC_assms]:
+  "\<lbrace>integrity aag X st and K (is_subject aag thread)\<rbrace>
+   handle_arch_fault_reply fault thread x y
+   \<lbrace>\<lambda>_. integrity aag X st\<rbrace>"
+  by (wpsimp simp: handle_arch_fault_reply_def)
+
+lemma auth_ipc_buffers_kheap_update[Ipc_AC_assms]:
+  "\<lbrakk> x \<in> auth_ipc_buffers st thread; kheap st thread = Some (TCB tcb);
+     kheap s thread = Some (TCB tcb'); tcb_ipcframe tcb = tcb_ipcframe tcb' \<rbrakk>
+     \<Longrightarrow> x \<in> auth_ipc_buffers (s\<lparr>kheap := (kheap s)(thread \<mapsto> TCB tcb)\<rparr>) thread"
+  by (clarsimp simp: auth_ipc_buffers_member_def get_tcb_def caps_of_state_tcb)
+
+lemma auth_ipc_buffers_machine_state_update[Ipc_AC_assms, simp]:
+  "auth_ipc_buffers (machine_state_update f s) = auth_ipc_buffers s"
+  by (clarsimp simp: auth_ipc_buffers_def get_tcb_def)
+
+lemma cap_insert_ext_integrity_asids_in_ipc[Ipc_AC_assms, wp]:
+  "cap_insert_ext src_parent src_slot dest_slot src_p dest_p
+   \<lbrace>\<lambda>s. integrity_asids aag subjects x asid st
+          (s\<lparr>kheap := \<lambda>a. if a = receiver then kheap st receiver else kheap s a\<rparr>)\<rbrace>"
+  by wpsimp
+
+lemma integrity_asids_kh_updI[Ipc_AC_assms]:
+  "integrity_asids_2 aag subjects x asid as as ao ao'
+   \<Longrightarrow> integrity_asids_2 aag subjects x asid as as (ao(p := ako)) (ao'(p := ako))"
+  by (clarsimp simp: integrity_asids_def opt_map_def)
+
+declare handle_arch_fault_reply_inv[Ipc_AC_assms]
+declare arch_get_sanitise_register_info_inv[Ipc_AC_assms]
+declare integrity_arch_triv[Ipc_AC_assms]
+
+end
+
+
+context is_extended begin interpretation Arch .
+
+lemma list_integ_lift_in_ipc[Ipc_AC_assms]:
+  assumes li:
+   "\<lbrace>list_integ (cdt_change_allowed aag {pasSubject aag} (cdt st) (tcb_states_of_state st)) st and Q\<rbrace>
+    f
+    \<lbrace>\<lambda>_. list_integ (cdt_change_allowed aag {pasSubject aag} (cdt st) (tcb_states_of_state st)) st\<rbrace>"
+  shows "\<lbrace>integrity_tcb_in_ipc aag X receiver epptr ctxt st and Q\<rbrace>
+         f
+         \<lbrace>\<lambda>_. integrity_tcb_in_ipc aag X receiver epptr ctxt st\<rbrace>"
+  apply (unfold integrity_tcb_in_ipc_def integrity_def[abs_def] pool_for_asid_def)
+  apply (simp del:split_paired_All)
+  apply (rule hoare_pre)
+   apply (simp only: integrity_cdt_list_as_list_integ)
+   apply (simp add: tcb_states_of_state_def get_tcb_def)
+   apply (wp li[simplified tcb_states_of_state_def get_tcb_def])+
+  apply (simp only: integrity_cdt_list_as_list_integ)
+  apply (simp add: tcb_states_of_state_def get_tcb_def)
+  apply (fastforce simp: opt_map_def)
+  done
+
+end
+
+
+global_interpretation Ipc_AC_2?: Ipc_AC_2
+proof goal_cases
+  interpret Arch .
+  case 1 show ?case
+    by (unfold_locales; (fact Ipc_AC_assms)?)
+qed
+
+end
