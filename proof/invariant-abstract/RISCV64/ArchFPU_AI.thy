6acec8521ea9d8eb7b041aaea238b09a5b0649ef
Corey Lewis <corey.lewis@proofcraft.systems>
2025-07-25 14:05:14 +1000

    ainvs: add ArchFPU_AI to AInvs session
    Also add placeholder files for architectures that do not enable
    the FPU.
    
    Signed-off-by: Corey Lewis <corey.lewis@proofcraft.systems>

---
+++ proof/invariant-abstract/RISCV64/ArchFPU_AI.thy
@@ -0,0 +1,13 @@
+(*
+ * Copyright 2025, Proofcraft Pty Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchFPU_AI
+imports ArchDetSchedSchedule_AI
+begin
+
+(* FIXME: placeholder, RISCV64 does not have FPU enabled *)
+
+end
