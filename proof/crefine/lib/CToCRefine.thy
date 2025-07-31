ebb04ad90c9e36a62864dd379807416346d96f84
Gerwin Klein <gerwin.klein@proofcraft.systems>
2025-07-31 11:55:26 +1000

    crefine lib: automatic substitute refinement
    Adjust the automatic refinement proof to work for Gammas where one
    function is overridden.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- proof/crefine/lib/CToCRefine.thy
+++ proof/crefine/lib/CToCRefine.thy
@@ -7,11 +7,17 @@
 theory CToCRefine
 
 imports
-    "CSpec.Substitute"
+    "CSpec.Multikernel_C"
     "CLib.SimplRewrite"
     Lib.Lib
     "CParser.TypHeapLib"
 begin
+
+lemma spec_statefn_simulates_fun_upd:
+  "\<lbrakk> exec_statefn_simulates g UNIV UNIV v v';
+     spec_statefn_simulates g G G' \<rbrakk>
+    \<Longrightarrow> spec_statefn_simulates g (G(p := Some v)) (G'(p := Some v'))"
+  by (simp add: spec_statefn_simulates_def)
 
 lemma spec_statefn_simulates_lookup_tree_Node:
   "\<lbrakk> exec_statefn_simulates g UNIV UNIV v v';
@@ -44,28 +50,22 @@ val unfold_bodies = Simplifier.make_simproc @{context}
 theorem spec_refine:
   notes if_split[split del]
   shows
-  "spec_statefn_simulates id (kernel_all_global_addresses.\<Gamma> symbol_table)
-     (kernel_all_substitute.\<Gamma> symbol_table domain)"
-  apply (simp add: kernel_all_global_addresses.\<Gamma>_def kernel_all_substitute.\<Gamma>_def)
+  "spec_statefn_simulates id (kernel_all_multi.\<Gamma> symbol_table cpuNum)
+     (kernel_all_substitute.\<Gamma> symbol_table domain cpuNum)"
+  apply (simp add: kernel_all_multi.\<Gamma>_def kernel_all_substitute.\<Gamma>_def)
+  apply (rule spec_statefn_simulates_fun_upd, rule exec_statefn_simulates_comI)
+  apply (simp add: kernel_all_global_addresses.\<Gamma>0_def kernel_all_substitute0.\<Gamma>0_def)
   apply (intro spec_statefn_simulates_lookup_tree_Node spec_statefn_simulates_lookup_tree_Leaf)
   apply (tactic \<open>ALLGOALS (asm_simp_tac (put_simpset HOL_ss @{context} addsimps @{thms switch.simps fst_conv snd_conv}
                   addsimprocs [unfold_bodies] |> Splitter.del_split @{thm if_split}))
               THEN ALLGOALS (TRY o resolve_tac @{context} @{thms exec_statefn_simulates_refl})\<close>)
-
   apply (tactic \<open>ALLGOALS (REPEAT_ALL_NEW (resolve_tac @{context} @{thms exec_statefn_simulates_comI
                       exec_statefn_simulates_additionals}))\<close>)
   apply (unfold id_apply)
   apply (tactic \<open>ALLGOALS (TRY o resolve_tac @{context} @{thms refl bij_id})\<close>)
   apply (tactic \<open>ALLGOALS (TRY o (resolve_tac @{context} @{thms subsetI} THEN' resolve_tac @{context} @{thms CollectI}
            THEN' REPEAT_ALL_NEW (eresolve_tac @{context} @{thms IntE CollectE conjE exE h_t_valid_c_guard conjI} ORELSE' assume_tac @{context})))\<close>)
-  (*
-    apply (tactic {* ALLGOALS (TRY o ((REPEAT_ALL_NEW (rtac @{thm c_guard_field}) THEN' etac @{thm h_t_valid_c_guard})
-                          THEN_ALL_NEW simp_tac @{simpset}
-                          THEN_ALL_NEW simp_tac @{simpset}
-                          THEN_ALL_NEW K no_tac))  *})
-  *)
   apply (rule bij_id[simplified id_def])+
   done (* Woo! *)
 
 end
-
