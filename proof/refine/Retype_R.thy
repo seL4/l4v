713b9a22c6b16d498b09128f57ed3c732870eaf3
Rafal Kolanski <rafal.kolanski@proofcraft.systems>
2025-11-26 01:42:21 +1100

    refine: Retype_R given Arch prefix
    In preparation for arch-split.
    Create Retype_R.thy and update import hierarchy.
    
    Signed-off-by: Rafal Kolanski <rafal.kolanski@proofcraft.systems>

---
+++ proof/refine/Retype_R.thy
@@ -0,0 +1,15 @@
+(*
+ * Copyright 2014, General Dynamics C4 Systems
+ * Copyright 2020, Data61, CSIRO (ABN 41 687 119 230)
+ * Copyright 2023, Proofcraft Pty Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+(* Retype refinement *)
+
+theory Retype_R
+imports ArchVSpace_R
+begin
+
+end
