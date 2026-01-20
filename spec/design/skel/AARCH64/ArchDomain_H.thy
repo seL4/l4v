1e87d6b78aac6d436e1433ebc6f83f22c490a7bc
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-01-20 15:46:20 +1100

    design+translator: dynamic domain scheduler
    - manually define basic time types and functions in
      Structs_B/Arch_Structs_B so they can be shared with ASpec
    
    - flip dependency order of Arch_Structs_B and Structs_B so that there is
      a generic ancestor that can defined the ticks type before
      the arch-specific definition of parseTimeArg that needs it
    
    - new skeleton files Domain_H and ArchDomain_H corresponding to the new
      Haskell Domain.hs files
    
    - mark domain_schedule_item as known type in the Haskell translator
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

---
+++ spec/design/skel/AARCH64/ArchDomain_H.thy
@@ -0,0 +1,18 @@
+(*
+ * Copyright 2026, Proofcraft Pty Ltd
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *)
+
+theory ArchDomain_H
+imports
+  FPU_H
+  VCPU_H
+begin
+
+context Arch begin arch_global_naming (H)
+
+#INCLUDE_HASKELL SEL4/Object/Domain/AARCH64.hs
+
+end
+end
--- spec/design/skel/RISCV64/Arch_Structs_B.thy
+++ spec/design/skel/RISCV64/Arch_Structs_B.thy
@@ -10,14 +10,13 @@ chapter "Common, Architecture-Specific Data Types"
 
 theory Arch_Structs_B
 imports
-  Setup_Locale
-  Lib.HaskellLib_H
-  MachineExports
+  Structs_B
 begin
 
 context Arch begin arch_global_naming (H)
 
 #INCLUDE_HASKELL SEL4/Model/StateData/RISCV64.hs CONTEXT RISCV64_H ONLY RISCVVSpaceRegionUse
+#INCLUDE_HASKELL SEL4/Object/Structures/RISCV64.hs ONLY parseTimeArg
 
 end
 
