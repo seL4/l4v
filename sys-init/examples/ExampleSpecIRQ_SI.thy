8e88cd40ac222fcfbe3eb56d51c610e8ddaea17f
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-02-23 17:26:33 +1100

    sysinit: dynamic domain scheduler
    Only update the example systems for now. The initialiser explicitly
    assumes that all TCBs are allocated to domain 0, so there is no point
    setting a domain schedule.
    
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- sys-init/examples/ExampleSpecIRQ_SI.thy
+++ sys-init/examples/ExampleSpecIRQ_SI.thy
@@ -205,7 +205,9 @@ definition
    cdl_current_thread = undefined,
    cdl_irq_node = example_irq_node,
    cdl_asid_table = undefined,
-   cdl_current_domain = minBound\<rparr>"
+   cdl_current_domain = minBound,
+   cdl_dom_schedule = [(0, maxBound)],
+   cdl_dom_start = 0\<rparr>"
 
 
 lemmas cnode_defs = cnode_a1_def cnode_a2_def cnode_b_def cnode_extra_def
@@ -897,4 +899,3 @@ lemma well_formed_example:
 end
 
 end
-
